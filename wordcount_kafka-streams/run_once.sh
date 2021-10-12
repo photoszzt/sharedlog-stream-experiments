c8d2cfbba932e41c3722dd0086b6e762d9e413ac
Zhiting Zhu <zhitingz@cs.utexas.edu>
2021-10-12 01:32:34 -0500

    backup working config
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- wordcount_kafka-streams/run_once.sh
+++ wordcount_kafka-streams/run_once.sh
@@ -15,7 +15,6 @@ $HELPER_SCRIPT generate-docker-compose --base-dir=$BASE_DIR
 scp -q $BASE_DIR/docker-compose.yml $MANAGER_HOST:~
 
 ssh -q $MANAGER_HOST -- docker stack rm kstreams-test || true
-ssh -q $MANAGER_HOST -- docker service rm app || true
 ssh -q $MANAGER_HOST -- docker service rm "kstreams-test_source" || true
 ssh -q $MANAGER_HOST -- docker service rm "kstreams-test_wordcount" || true
 
@@ -38,6 +37,20 @@ ssh -q $MANAGER_HOST -- SRC_DIR=$SRC_DIR \
     -c ~/docker-compose-base.yml -c ~/docker-compose.yml kstreams-test
 sleep 80
 
+FIRST_BROKER_CONTAINER_IP=""
+for HOST in $ALL_BROKER_HOSTS; do
+    container_id=$(ssh -q $HOST -oStrictHostKeyChecking=no -- "docker ps -f name=kstreams-test_broker --format "{{.ID}}"")
+    docker_ip_outside=$(ssh -q $HOST -oStrictHostKeyChecking=no -- "docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id")
+    docker_ip_inside=$(ssh -q $HOST -oStrictHostKeyChecking=no -- "docker exec $container_id hostname -i")
+    if [ $docker_ip_inside != $docker_ip_outside ]; then
+        echo "container $HOST ip observed from outside: $docker_ip_outside, from inside: $docker_ip_inside are different"
+        exit -1
+    fi
+    if [ $HOST = $FIRST_BROKER ]; then
+        FIRST_BROKER_CONTAINER_IP=$docker_ip_outside
+    fi
+done
+
 rm -rf $EXP_DIR
 mkdir -p $EXP_DIR
 ssh -q $MANAGER_HOST -- cat /proc/cmdline >>$EXP_DIR/kernel_cmdline
@@ -45,15 +58,15 @@ ssh -q $MANAGER_HOST -- uname -a >>$EXP_DIR/kernel_version
 
 ssh -q $MANAGER_HOST -- "docker service create \
     --mount type=bind,source=/mnt/efs/workspace/sharedlog-stream,destination=/src \
-    --constraint node.role==manager --network kstreams-test_default \
+    --constraint node.labels.source_node==true --network kstreams-test_default \
     --name kstreams-test_source --restart-condition none ubuntu:focal /src/bin/wordcount_genevents_kafka \
-    -broker broker-1:9092 -in_fname /src/data/books.dat"
+    -broker $FIRST_BROKER_CONTAINER_IP:9092 -in_fname /src/data/books.dat"
 
-sleep 10
+sleep 30
 
 ssh -q $MANAGER_HOST -- "docker service create \
     --mount type=bind,source=/mnt/efs/workspace/kafka-streams-benchmark/wordcount,destination=/src \
-    --constraint node.labels.app_node==true --env BOOTSTRAP_SERVER_CONFIG=$FIRST_BROKER:29092 \
+    --constraint node.labels.app_node==true --env BOOTSTRAP_SERVER_CONFIG=$FIRST_BROKER_CONTAINER_IP:9092 \
     --network kstreams-test_default --restart-condition none \
     --name kstreams-test_wordcount openjdk:11.0.12-jre-slim-buster \
     bash -c 'java -jar /src/target/wordcount-1.0-SNAPSHOT-jar-with-dependencies.jar'"
