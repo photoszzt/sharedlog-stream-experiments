edbc92b20344330fdc4c072f903a0161528a9cdc
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-04-19 22:58:08 -0500

    update run all script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-TRAN_DIR=4_2xlarge_tran
-NORM_DIR=4_2xlarge
+DURATION=1500
+EVENTS_NUM=6250000
+TRAN_DIR=4_2xlarge_tran_${DURATION}_6.25M
+NORM_DIR=4_2xlarge_${DURATION}_6.25M
 cd q3_boki
 cd mongodb
 ../../../workspace/research-helper-scripts/microservice_helper start-machines --use-spot-instances
@@ -18,56 +20,56 @@ cp q3_boki/mongodb/machines.json ./q8_boki/mem
 cp q3_boki/mongodb/machines.json ./q8_boki/mongodb
 
 # cd q1_boki
-# ./run_once.sh ./4src/$NORM_DIR false
-# ./run_once.sh ./4src/$TRAN_DIR/ true
+# ./run_once.sh ./4src/$NORM_DIR false $DURATION $EVENTS_NUM
+# ./run_once.sh ./4src/$TRAN_DIR/ true $DURATION $EVENTS_NUM
 # cd ..
 # 
 # cd q2_boki
-# ./run_once.sh ./4src/$NORM_DIR false
-# ./run_once.sh ./4src/$TRAN_DIR/ true
+# ./run_once.sh ./4src/$NORM_DIR false $DURATION $EVENTS_NUM
+# ./run_once.sh ./4src/$TRAN_DIR/ true $DURATION $EVENTS_NUM
 # cd ..
-# 
-# cd q3_boki
-# cd mem
-# ./run_once.sh ./4src/$NORM_DIR false
-# ./run_once.sh ./4src/$TRAN_DIR/ true
-# cd ..
-# cd mongodb
-# ./run_once.sh ./4src/$NORM_DIR false
-# ./run_once.sh ./4src/$TRAN_DIR/ true
-# cd ..
-# cd ..
+
+cd q3_boki
+cd mem
+./run_once.sh ./4src/$NORM_DIR false $DURATION $EVENTS_NUM
+./run_once.sh ./4src/$TRAN_DIR/ true $DURATION $EVENTS_NUM
+cd ..
+cd mongodb
+./run_once.sh ./4src/$NORM_DIR false $DURATION $EVENTS_NUM
+./run_once.sh ./4src/$TRAN_DIR/ true $DURATION $EVENTS_NUM
+cd ..
+cd ..
 
 cd q5_boki
 cd mem
-./run_once.sh ./4src/${NORM_DIR}/ false
-./run_once.sh ./4src/${TRAN_DIR}/ true
+./run_once.sh ./4src/${NORM_DIR}/ false $DURATION $EVENTS_NUM
+./run_once.sh ./4src/${TRAN_DIR}/ true $DURATION $EVENTS_NUM
 cd ..
 cd mongodb
-./run_once.sh ./4src/${NORM_DIR}/ false
-./run_once.sh ./4src/${TRAN_DIR}/ true
+./run_once.sh ./4src/${NORM_DIR}/ false $DURATION $EVENTS_NUM
+./run_once.sh ./4src/${TRAN_DIR}/ true $DURATION $EVENTS_NUM
 cd ..
 cd ..
 
-cd q7_boki
-cd mem
-./run_once.sh ./4src/${NORM_DIR}/ false
-./run_once.sh ./4src/${TRAN_DIR}/ true
-cd ..
-cd mongodb
-./run_once.sh ./4src/${NORM_DIR}/ false
-./run_once.sh ./4src/${TRAN_DIR}/ true
-cd ..
-cd ..
+# cd q7_boki
+# cd mem
+# ./run_once.sh ./4src/${NORM_DIR}/ false
+# ./run_once.sh ./4src/${TRAN_DIR}/ true
+# cd ..
+# cd mongodb
+# ./run_once.sh ./4src/${NORM_DIR}/ false
+# ./run_once.sh ./4src/${TRAN_DIR}/ true
+# cd ..
+# cd ..
 
 cd q8_boki
 cd mem
-./run_once.sh ./4src/${NORM_DIR} false
-./run_once.sh ./4src/${TRAN_DIR}/ true
+./run_once.sh ./4src/${NORM_DIR} false $DURATION $EVENTS_NUM
+./run_once.sh ./4src/${TRAN_DIR}/ true $DURATION $EVENTS_NUM
 cd ..
 cd mongodb
-./run_once.sh ./4src/$NORM_DIR false
-./run_once.sh ./4src/$TRAN_DIR/ true
+./run_once.sh ./4src/$NORM_DIR false $DURATION $EVENTS_NUM
+./run_once.sh ./4src/$TRAN_DIR/ true $DURATION $EVENTS_NUM
 cd ..
 cd ..
 
