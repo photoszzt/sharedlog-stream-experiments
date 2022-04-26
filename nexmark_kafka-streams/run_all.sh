d29ea24fe92d38f29d30e748f09e53e4a148fe5f
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-04-26 12:22:19 -0500

    use absolute path
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_kafka-streams/run_all.sh
+++ nexmark_kafka-streams/run_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-../../workspace/research-helper-scripts/microservice_helper start-machines --use-spot-instances
+/mnt/efs/workspace/research-helper-scripts/microservice_helper start-machines --use-spot-instances
 DURATION=200
 ./nexmark.sh --app q1 --exp_dir ./q1/4src/4_2xlarge_tran_${DURATION}/ --nins 4 --nsrc 4 --serde msgp --duration $DURATION 
 ./nexmark.sh --app q2 --exp_dir ./q2/4src/4_2xlarge_tran_${DURATION}/ --nins 4 --nsrc 4 --serde msgp --duration $DURATION 
@@ -8,4 +8,4 @@ DURATION=200
 # ./nexmark.sh --app q7 --exp_dir ./q7/4src/4_2xlarge_tran/ --nins 4 --nsrc 4 --serde msgp --duration 300
 ./nexmark.sh --app q8 --exp_dir ./q8/4src/4_2xlarge_tran_${DURATION}/ --nins 4 --nsrc 4 --serde msgp --duration $DURATION
 
-../../workspace/research-helper-scripts/microservice_helper stop-machines
+/mnt/efs/workspace/research-helper-scripts/microservice_helper stop-machines
