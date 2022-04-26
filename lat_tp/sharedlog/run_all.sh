a48248094d005e034fccc63a993bc2d96f727a78
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-04-26 10:33:55 -0500

    add config
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ lat_tp/sharedlog/run_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/mnt/efs/workspace/research-helper-scripts/microservice_helper start-machines --use-spot-instances
+./1prod_1t_1par_100b.sh
+./1prod_1t_1par_1kb.sh
+./1prod_1t_1par_4kb.sh
+./1prod_1t_1par_16kb.sh
+/mnt/efs/workspace/research-helper-scripts/microservice_helper stop-machines
---
+++ lat_tp/sharedlog/run_once.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
+if [ "$1" = "" ]; then
+    echo "should provide exp_dir"
+    exit 1
+fi
+
+EXP_DIR=""
+DURATION=""
+EVENTS_NUM=""
+NUM_PARTITION=""
+NUM_PRODUCER=""
+PAYLOAD=""
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    --exp_dir*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        EXP_DIR="${1#*=}"
+        ;;
+    --duration*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        DURATION="${1#*=}"
+        ;;
+    --events_num*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        EVENTS_NUM="${1#*=}"
+        ;;
+    --npar*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        NUM_PARTITION="${1#*=}"
+        ;;
+    --nprod*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        NUM_PRODUCER="${1#*=}"
+        ;;
+    --payload*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        PAYLOAD="${1#*=}"
+        ;;
+    --help | -h)
+        printf -- "--exp_dir <exp_dir> required\n"
+        printf -- "--duration <duration in sec>\n"
+        printf -- "--events_num <number of events>\n"
+        exit 0
+        ;;
+    *)
+        printf >&2 "Error: invalid argument"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+DURATION=${DURATION:-60}
+echo "exp_dir: $EXP_DIR, num_producer: $NUM_PRODUCER, duration: $DURATION, num partition: $NUM_PARTITION, payload: $PAYLOAD"
+
+if [[ "$NUM_EVENTS" = "" ]]; then
+    echo "need to specify number of events"
+    exit 1
+fi
+if [[ "$EXP_DIR" = "" ]]; then
+    echo "need to specify experiment dir"
+    exit 1
+fi
+if [[ "$NUM_PRODUCER" = "" ]]; then
+    echo "need to specify number of producer"
+    exit 1
+fi
+if [[ "$NUM_PARTITION" = "" ]]; then
+    echo "need to specify number of partition"
+    exit 1
+fi
+if [[ "$PAYLOAD" = "" ]]; then
+    echo "need to specify the payload"
+    exit 1
+fi
+
+
+echo "exp_dir: $EXP_DIR, num_producer: $NUM_PRODUCER, duration: $DURATION, num partition: $NUM_PARTITION, payload: $PAYLOAD"
+
+HELPER_SCRIPT=/mnt/efs/workspace/research-helper-scripts/microservice_helper
+BASE_DIR=$(realpath $(dirname $0))
+SRC_DIR=/mnt/efs/workspace/sharedlog-stream
+MANAGER_HOST=$($HELPER_SCRIPT get-docker-manager-host --base-dir=$BASE_DIR)
+CLIENT_HOST=$($HELPER_SCRIPT get-client-host --base-dir=$BASE_DIR)
+ENTRY_HOST=$($HELPER_SCRIPT get-service-host --base-dir=$BASE_DIR --service=faas-gateway)
+
+./run_once_common.sh
+
+rm -rf $EXP_DIR
+mkdir -p $EXP_DIR
+
+ssh -q $MANAGER_HOST -- cat /proc/cmdline >>$EXP_DIR/kernel_cmdline
+ssh -q $MANAGER_HOST -- uname -a >>$EXP_DIR/kernel_version
+
+ssh -q $CLIENT_HOST -- $SRC_DIR/bin/sharedlog_bench_client \
+    -faas_gateway $ENTRY_HOST:8080 -duration ${DURATION} -events_num ${EVENTS_NUM} -serde msgp \
+    -stat_dir ./${EXP_DIR}/stats -npar ${NUM_PARTITION} -nprod ${NUM_PRODUCER} \
+    -payload $SRC_DIR/data/${PAYLOAD} >$EXP_DIR/results.log 2>&1
+
+ssh -q $CLIENT_HOST -- "for i in $(ls /home/ubuntu/${APP_NAME}/${EXP_DIR}/stats); do gzip -9 /home/ubuntu/${APP_NAME}/${EXP_DIR}/stats/$i; done"
+
+scp -r $CLIENT_HOST:/home/ubuntu/${APP_NAME}/${EXP_DIR}/stats ${EXP_DIR}
+
+$HELPER_SCRIPT collect-container-logs --base-dir=$BASE_DIR --log-path=$EXP_DIR/logs
