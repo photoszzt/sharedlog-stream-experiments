183f6c587184d9345fc6d5a472729a38fc78791c
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-05-04 10:54:24 -0500

    backup changes
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- lat_tp/kafka/1prod_1t_1par_1kb.sh
+++ lat_tp/kafka/1prod_1t_1par_1kb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./produce_bench.sh --exp_dir 1p_1t_1par_1kb --ncon 1 --nprod 1 --duration 120 --events_num 50000000 --num_par 1 --payload payload-1Kb.data
+./produce_bench.sh --exp_dir 1p_1t_1par_1kb --ncon 1 --nprod 1 --duration 120 --events_num 25000000 --num_par 1 --payload payload-1Kb.data
--- lat_tp/kafka/run_all.sh
+++ lat_tp/kafka/run_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+NUM_EVENTS=25000000
 /mnt/efs/workspace/research-helper-scripts/microservice_helper start-machines --use-spot-instances
-./1prod_1t_1par_100b.sh
-./1prod_1t_1par_1kb.sh
-./1prod_1t_1par_4kb.sh
-./1prod_1t_1par_16kb.sh
+./produce_bench.sh --exp_dir 1p_1t_1par_100b --ncon 1 --nprod 1 --duration 120 --events_num 25000000 --num_par 1 --payload payload-100b.data
+./produce_bench.sh --exp_dir 1p_1t_1par_1kb --ncon 1 --nprod 1 --duration 120 --events_num 25000000 --num_par 1 --payload payload-1Kb.data
+./produce_bench.sh --exp_dir 1p_1t_1par_4kb --ncon 1 --nprod 1 --duration 120 --events_num 25000000 --num_par 1 --payload payload-4Kb.data
+./produce_bench.sh --exp_dir 1p_1t_1par_16kb --ncon 1 --nprod 1 --duration 120 --events_num 25000000 --num_par 1 --payload payload-16Kb.data
 /mnt/efs/workspace/research-helper-scripts/microservice_helper stop-machines
