deff2d6191471a8dc037cd6e7449de90fb1ad945
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-05-05 01:55:03 -0500

    add warmup config
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- lat_tp/sharedlog/run_all.sh
+++ lat_tp/sharedlog/run_all.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 /mnt/efs/workspace/research-helper-scripts/microservice_helper start-machines --use-spot-instances
-./1prod_1t_1par_100b.sh
-./1prod_1t_1par_1kb.sh
-./1prod_1t_1par_4kb.sh
-./1prod_1t_1par_16kb.sh
+
+TPS=(100 1000 10000)
+DURATION=180
+WARM_DURATION=60
+WARM_EVENTS=(6000 60000 600000)
+EVENTS=(18000 180000 1800000)
+payload=(100b 1Kb 4Kb 16Kb)
+for ((idx=1; idx<=${#TPS[@]}; ++idx)); do
+    for ((j=1; j<=${#payload[@]}; ++j)); do
+        ./run_once.sh --exp_dir 180s_60swarm/1prod_1t_1par_5ms_${TPS[idx]}/${payload[j]} \
+            --tps ${TPS[idx]} --warm_duration $WARM_DURATION --warm_events ${WARM_EVENTS[idx]} \
+            --duration $DURATION --events_num ${EVENTS[idx]} \
+            --npar 1 --nprod 1 --payload payload-${payload[j]}.data
+    done
+done
 /mnt/efs/workspace/research-helper-scripts/microservice_helper stop-machines
--- lat_tp/sharedlog/run_once.sh
+++ lat_tp/sharedlog/run_once.sh
@@ -13,6 +13,10 @@ EVENTS_NUM=""
 NUM_PARTITION=""
 NUM_PRODUCER=""
 PAYLOAD=""
+WARM_DURATION=""
+WARM_EVENTS=""
+TPS=""
+
 
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -39,6 +43,18 @@ while [ $# -gt 0 ]; do
     --payload*)
         if [[ "$1" != *=* ]]; then shift; fi
         PAYLOAD="${1#*=}"
+        ;;
+    --warm_duration*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        WARM_DURATION="${1#*=}"
+        ;;
+    --warm_events*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        WARM_EVENTS="${1#*=}"
+        ;;
+    --tps*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        TPS="${1#*=}"
         ;;
     --help | -h)
         printf -- "--exp_dir <exp_dir> required\n"
@@ -77,9 +93,21 @@ if [[ "$PAYLOAD" = "" ]]; then
     echo "need to specify the payload"
     exit 1
 fi
+if [[ "$WARM_EVENTS" = "" ]]; then
+    echo "need to specify number of warmup events"
+    exit 1
+fi
+if [[ "$WARM_DURATION" = "" ]]; then
+    echo "need to specify warmup duration"
+    exit 1
+fi
+if [[ "$TPS" = "" ]]; then
+    echo "need to specify tps"
+    exit 1
+fi
 
 
-echo "exp_dir: $EXP_DIR, num_producer: $NUM_PRODUCER, duration: $DURATION, num partition: $NUM_PARTITION, payload: $PAYLOAD"
+echo "exp_dir: $EXP_DIR, num_producer: $NUM_PRODUCER, duration: $DURATION, num partition: $NUM_PARTITION, payload: $PAYLOAD, warm_duration: $WARM_DURATION, warm_events: $WARM_EVENTS, tps: $TPS"
 
 HELPER_SCRIPT=/mnt/efs/workspace/research-helper-scripts/microservice_helper
 BASE_DIR=$(realpath $(dirname $0))
@@ -99,6 +127,7 @@ ssh -q $MANAGER_HOST -- uname -a >>$EXP_DIR/kernel_version
 ssh -q $CLIENT_HOST -- $SRC_DIR/bin/sharedlog_bench_client \
     -faas_gateway $ENTRY_HOST:8080 -duration ${DURATION} -events_num ${EVENTS_NUM} -serde msgp \
     -npar ${NUM_PARTITION} -nprod ${NUM_PRODUCER} \
-    -payload /src/data/${PAYLOAD} >$EXP_DIR/results.log 2>&1
+    -payload /src/data/${PAYLOAD} -tps $TPS -warmup_events $WARM_EVENTS 
+    -warm_time $WARM_DURATION \ >$EXP_DIR/results.log 2>&1
 
 $HELPER_SCRIPT collect-container-logs --base-dir=$BASE_DIR --log-path=$EXP_DIR/logs
