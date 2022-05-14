38431ce749bc89db818a4612eba5d4b4e2f0fc15
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-05-13 22:29:05 -0500

    publish port
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_kafka-streams/nexmark.sh
+++ nexmark_kafka-streams/nexmark.sh
@@ -158,8 +158,8 @@ ssh -q $MANAGER_HOST -- "docker service create \
     --mount type=bind,source=/mnt/efs/workspace/sharedlog-stream,destination=/src \
     --constraint node.labels.source_node==true --network kstreams-test_default \
     --name kstreams-test_source --restart-condition none --replicas=$NUM_SRC_INSTANCE \
-    --replicas-max-per-node=1 --hostname='source-{{.Task.Slot}}' --env IID='{{.Task.Slot}}' ubuntu:focal \
-    /src/bin/nexmark_genevents_kafka \
+    --replicas-max-per-node=1 --hostname='source-{{.Task.Slot}}' --publish published=8080,target=8080 \
+    --env IID='{{.Task.Slot}}' ubuntu:focal /src/bin/nexmark_genevents_kafka \
     -broker $FIRST_BROKER_CONTAINER_IP:9092 -duration ${SRC_DURATION} -npar 4 -serde $SERDE \
     -srcIns $NUM_SRC_INSTANCE -events_num $NUM_EVENTS -tps $TPS" &
 
@@ -169,7 +169,7 @@ ssh -q $MANAGER_HOST -- "docker service create \
     --env BOOTSTRAP_SERVER_CONFIG=$FIRST_BROKER_CONTAINER_IP:9092 \
     --network kstreams-test_default --restart-condition none --replicas=$NUM_INSTANCE \
     --replicas-max-per-node=1 --hostname='nexmark-{{.Task.Slot}}' \
-    --name kstreams-test_nexmark openjdk:11.0.12-jre-slim-buster \
+    --name kstreams-test_nexmark --publish published=8090,target=8090 openjdk:11.0.12-jre-slim-buster \
     bash -c 'java -cp /src/build/libs/nexmark-kafka-streams-0.2-SNAPSHOT-uber.jar com.github.nexmark.kafka.queries.RunQuery \
     --name $NAME --serde $SERDE --srcEvents $NUM_EVENTS --conf  /src/workload_config/${NAME}.properties --duration $DURATION \
     --warmup_time $WARM_DURATION'"
