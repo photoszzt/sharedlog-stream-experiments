5fe8916bf27b8c1947a8204239b6e24afca2e11e
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-08-01 15:42:15 -0500

    split dir
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_kafka-streams/run_all.sh
+++ nexmark_kafka-streams/run_all.sh
@@ -12,11 +12,11 @@ for ((j=0; j<${#APP[@]}; ++j)); do
 	EVENTS=$(expr ${TPS[idx]} \* $DURATION)
         echo ${APP[j]}, ${EVENTS} events, ${TPS[idx]} tps
     	./nexmark.sh --app ${APP[j]} \
-    	    --exp_dir ./${APP[j]}/4src_ets/${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms/${TPS[idx]}tps_${EVENTS}/ \
+    	    --exp_dir ./${APP[j]}/4src_ets/${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms/${TPS[idx]}tps_${EVENTS}_alo/ \
     	    --nins 4 --nsrc 4 --serde msgp --duration $DURATION --nevents ${EVENTS} \
     	    --tps ${TPS[idx]} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS --gua alo
     	./nexmark.sh --app ${APP[j]} \
-    	    --exp_dir ./${APP[j]}/4src_ets/${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms/${TPS[idx]}tps_${EVENTS}/ \
+    	    --exp_dir ./${APP[j]}/4src_ets/${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms/${TPS[idx]}tps_${EVENTS}_eo/ \
     	    --nins 4 --nsrc 4 --serde msgp --duration $DURATION --nevents ${EVENTS} \
     	    --tps ${TPS[idx]} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS --gua eo
     done
