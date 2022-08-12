860483606fcd5b113f21bfac07d82f8c1dbe7aaf
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-08-12 16:10:51 -0500

    backup run script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ nexmark_kafka-streams/run_q6.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+/mnt/efs/workspace/research-helper-scripts/microservice_helper start-machines --use-spot-instances
+
+TPS_PER_WORKER=(125 250 500)
+DURATION=180
+WARM_DURATION=0
+APP=(q6)
+FLUSH_MS=100
+NUM_INS=4
+
+for ((j=0; j<${#APP[@]}; ++j)); do
+    for ((idx=0; idx<${#TPS_PER_WORKER[@]}; ++idx)); do
+	TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_INS})
+	EVENTS=$(expr ${TPS} \* $DURATION)
+        echo ${APP[j]}, ${EVENTS} events, ${TPS} tps
+    	./nexmark.sh --app ${APP[j]} \
+    	    --exp_dir ./${APP[j]}/4src_ets/${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms/${TPS_PER_WORKER[idx]}tps_${EVENTS}_alo/ \
+    	    --nins 4 --nsrc 4 --serde msgp --duration $DURATION --nevents ${EVENTS} \
+    	    --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS --gua alo
+    	./nexmark.sh --app ${APP[j]} \
+    	    --exp_dir ./${APP[j]}/4src_ets/${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms/${TPS_PER_WORKER[idx]}tps_${EVENTS}_eo/ \
+    	    --nins 4 --nsrc 4 --serde msgp --duration $DURATION --nevents ${EVENTS} \
+    	    --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS --gua eo
+    done
+done
+
+# /mnt/efs/workspace/research-helper-scripts/microservice_helper stop-machines
