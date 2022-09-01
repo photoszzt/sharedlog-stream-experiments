e9eab46b4d1f9b3e4ad015a7eaaecbcc7a04512d
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-09-01 00:14:10 -0500

    update script path
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_kafka-streams/run_q8.sh
+++ nexmark_kafka-streams/run_q8.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-/mnt/efs/workspace/research-helper-scripts/microservice_helper start-machines --use-spot-instances
+set -x
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+WORKSPACE_DIR=$(realpath $SCRIPT_DIR/../../)
+$WORKSPACE_DIR/research-helper-scripts/microservice_helper start-machines --use-spot-instances
 
 TPS_PER_WORKER=(8000 16000)
 DURATION=180
@@ -14,15 +17,16 @@ for ((j=0; j<${#APP[@]}; ++j)); do
 	TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_INS})
 	EVENTS=$(expr ${TPS} \* $DURATION)
         echo ${APP[j]}, ${EVENTS} events, ${TPS} tps
+	subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms
     	./nexmark.sh --app ${APP[j]} \
-    	    --exp_dir ./${APP[j]}/4src_ets/${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms/${TPS_PER_WORKER[idx]}tps_${EVENTS}_alo/ \
+    	    --exp_dir ./${APP[j]}/4src_ets/$subdir/${TPS_PER_WORKER[idx]}tps_${EVENTS}_alo/ \
     	    --nins 4 --nsrc 4 --serde msgp --duration $DURATION --nevents ${EVENTS} \
     	    --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS --src_flushms ${SRC_FLUSH_MS} --gua alo
     	./nexmark.sh --app ${APP[j]} \
-    	    --exp_dir ./${APP[j]}/4src_ets/${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms/${TPS_PER_WORKER[idx]}tps_${EVENTS}_eo/ \
+    	    --exp_dir ./${APP[j]}/4src_ets/$subdir/${TPS_PER_WORKER[idx]}tps_${EVENTS}_eo/ \
     	    --nins 4 --nsrc 4 --serde msgp --duration $DURATION --nevents ${EVENTS} \
     	    --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS --src_flushms ${SRC_FLUSH_MS} --gua eo
     done
 done
 
-/mnt/efs/workspace/research-helper-scripts/microservice_helper stop-machines
+$WORKSPACE_DIR/research-helper-scripts/microservice_helper stop-machines
