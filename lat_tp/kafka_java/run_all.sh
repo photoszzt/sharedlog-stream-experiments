5ba613af9460052d4c87f3401c781c4b5e445c6e
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-10-03 23:05:04 -0500

    add missing config
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- lat_tp/kafka_java/run_all.sh
+++ lat_tp/kafka_java/run_all.sh
@@ -5,7 +5,7 @@ TPS=(10 50 100)
 DURATION=180
 payload=(16Kb)
 
-for ((iter = 0; iter < 5; ++iter)); do
+for ((iter = 0; iter < 3; ++iter)); do
     for ((idx = 0; idx < ${#TPS[@]}; ++idx)); do
         for ((j = 0; j < ${#payload[@]}; ++j)); do
             EVENTS=$(expr $DURATION \* ${TPS[idx]})
