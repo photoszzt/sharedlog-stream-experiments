75e6898ee19d7df9c04fa2050af23a79e47f85e8
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-10-03 21:38:49 -0500

    back up run script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- lat_tp/kafka/run_all.sh
+++ lat_tp/kafka/run_all.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 /mnt/efs/workspace/research-helper-scripts/microservice_helper start-machines --use-spot-instances
 
-TPS=(100 1000 10000)
+TPS=(10 50 100)
 DURATION=180
-WARM_DURATION=60
-WARM_EVENTS=(6000 60000 600000)
-EVENTS=(18000 180000 1800000)
-payload=(100b 1Kb 4Kb 16Kb)
-for ((idx=0; idx<${#TPS[@]}; ++idx)); do
-    for ((j=0; j<${#payload[@]}; ++j)); do
-        echo ${TPS[idx]}, ${payload[j]}, ${EVENTS[idx]}, ${WARM_EVENTS[idx]}
-        ./produce_bench.sh --exp_dir 180s_60swarm/1p_1t_1par_5ms_${TPS[idx]}tps/${payload[j]} \
-            --ncon 1 --nprod 1 --duration $DURATION \
-            --events_num ${EVENTS[idx]} --num_par 1 --payload payload-${payload[j]}.data \
-            --tps ${TPS[idx]} --warm_duration $WARM_DURATION --warm_events ${WARM_EVENTS[idx]}
+WARM_DURATION=0
+WARM_EVENTS=(0 0 0)
+payload=(16Kb)
+
+for ((iter=0; iter<5; ++iter)); do
+    for ((idx=0; idx<${#TPS[@]}; ++idx)); do
+        for ((j=0; j<${#payload[@]}; ++j)); do
+            EVENTS=$(expr $DURATION \* ${TPS[idx]})
+            echo ${TPS[idx]}, ${payload[j]}, ${EVENTS}, ${WARM_EVENTS[idx]}
+            ./produce_bench.sh --exp_dir 180s_0swarm/1p_1t_1par_${TPS[idx]}tps/$iter/${payload[j]} \
+                --ncon 1 --nprod 1 --duration $DURATION \
+                --events_num ${EVENTS} --num_par 1 --payload payload-${payload[j]}.data \
+                --tps ${TPS[idx]} --warm_duration $WARM_DURATION --warm_events ${WARM_EVENTS[idx]}
+        done
     done
 done
+
 /mnt/efs/workspace/research-helper-scripts/microservice_helper stop-machines
