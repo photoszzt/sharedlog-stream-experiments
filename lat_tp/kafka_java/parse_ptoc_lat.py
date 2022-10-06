733f6429d9ddf47d8c4c11fe8e722ffc92f8ccfd
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-10-06 16:05:25 -0500

    backup scripts
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ lat_tp/kafka_java/parse_ptoc_lat.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+import json
+from statistics import quantiles
+
+
+def parse_file(fpath: str):
+    with open(fpath, "r") as f:
+        for line in f:
+            if "[" in line:
+                arr = line.strip().split(", ")
+                arr[0] = arr[0][1:]
+                arr[-1] = arr[-1][:-1]
+                times = [ int(i) for i in arr]
+                return times
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", type=str, required=True)
+    args = parser.parse_args()
+    for root, dirs, files in os.walk(args.dir):
+        for fname in files:
+            if "consume" in fname and "stdout" in fname:
+                path = os.path.join(root, fname)
+                times = parse_file(path)
+                p50 = quantiles(times, n=100)[49]
+                p99 = quantiles(times, n=100)[98]
+                print(f"{path} p50: {p50} us, p99: {p99} us")
+
+
+if __name__ == '__main__':
+    main()
