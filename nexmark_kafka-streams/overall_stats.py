e5205769220a91100e7e7521711bf639517ceda0
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-10-16 23:09:38 -0500

    overall script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ nexmark_kafka-streams/overall_stats.py
@@ -0,0 +1,46 @@
+import argparse
+from pathlib import Path
+import pickle
+import numpy as np
+from parse_stage_time import stages
+
+throughput = {
+    "q3": [],
+    "q4": [],
+    "q5": [24000, 32000, 40000],
+    "q6": [],
+    "q7": [12000, 16000],
+    "q8": [],
+}
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", type=str, help="dir to parse", required=True)
+    parser.add_argument("--app", type=str, help="app to parse", required=True)
+    args = parser.parse_args()
+    tp_stats = {}
+    for tp in throughput[args.app]:
+        all_stats = {}
+        for fname in Path(args.dir).glob(f"{tp}_*.pickle"):
+            with open(fname, "rb") as f:
+                stats = pickle.load(f)
+                s = stages[args.app]
+                for st in s:
+                    if st not in all_stats:
+                        all_stats[st] = stats[st]
+                    else:
+                        all_stats[st].extend(stats[st])
+        for st in stages[args.app]:
+            data_arr = np.concatenate(all_stats[st])
+            all_stats[st] = data_arr
+        tp_stats[tp] = all_stats
+
+    for st in stages[args.app]:
+        for tp in throughput[args.app]:
+            data_arr = tp_stats[tp][st]
+            print(f"{tp},{st},{np.mean(data_arr)},{np.std(data_arr)},{np.min(data_arr)},{np.quantile(data_arr, 0.25)},{np.quantile(data_arr, 0.5)},{np.quantile(data_arr, 0.9)},{np.quantile(data_arr, 0.99)},{np.max(data_arr)}")
+
+
+if __name__ == '__main__':
+    main()
