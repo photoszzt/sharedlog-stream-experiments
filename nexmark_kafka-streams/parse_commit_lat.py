88cb402d0cf96515057ba0b624708ea90435eb90
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-10-25 15:08:55 -0500

    backup script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_kafka-streams/parse_commit_lat.py
+++ nexmark_kafka-streams/parse_commit_lat.py
@@ -1,30 +1,44 @@
 import argparse
 import os
+from pathlib import Path
 from statistics import mean
+
+stats_name = {
+        "commit-latency-max",
+        "commit-latency-avg",
+}
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', required=True)
-    parser.add_argument('--metric', required=True)
     args = parser.parse_args()
     stats = {}
-    for root, dirs, files in os.walk(args.dir):
-        for fname in files:
-            if "kstreams-test_nexmark" in fname and "stdout" in fname and fname not in stats:
-                fpath = os.path.join(root, fname)
-                stats[fname] = []
-                with open(fpath, "r") as f:
-                    for line in f:
-                        if args.metric in line:
-                            l = line.strip().split(";")
+    dirs_dict = {}
+    for root, dirs, _ in os.walk(args.dir):
+        for d in dirs:
+            if "eo" in d:
+                tps_per_work = int(d.split("_")[0][:-3])
+                dirs_dict[tps_per_work] = os.path.join(root, d, "logs")
+    for tps_per_work, dirpath in dirs_dict.items():
+        stats[tps_per_work] = {}
+        for fname in Path(dirpath).glob("*nexmark*.stdout"):
+            with open(fname, "r") as f:
+                for line in f:
+                    if "metrics" in line and ";" in line:
+                        l = line.strip().split(";")
+                        sp = l[0].split(" ")
+                        name = sp[1]
+                        if name in stats_name:
                             latStr = l[2].split(":")[1]
                             if "NaN" not in latStr:
-                                stats[fname].append(float(latStr))
-    all_commit_lat = []
-    for f, n in stats.items():
-        all_commit_lat.extend(n)
-    print(f"mean: {mean(all_commit_lat)}")
+                                if name not in stats[tps_per_work]:
+                                    stats[tps_per_work][name] = []
+                                stats[tps_per_work][name].append(float(latStr))
+    for tps_per_work, stat in stats.items():
+        for st_name, data in stat.items():
+            m = mean(data)
+            print(f"{tps_per_work},{st_name},{m}")
 
 
 
