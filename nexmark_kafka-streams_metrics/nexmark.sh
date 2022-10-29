09e1f00d2bb041f9844497fb05b6ee9fdcaff232
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-10-29 14:01:24 -0500

    change metrics related files to metrics folder; remove node exporter
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_kafka-streams_metrics/nexmark.sh
+++ nexmark_kafka-streams_metrics/nexmark.sh
@@ -14,6 +14,7 @@ FLUSH_MS=""
 SRC_FLUSH_MS=""
 GUARANTEE=""
 DISABLE_CACHE=""
+DISABLE_BATCHING=""
 
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -69,6 +70,10 @@ while [ $# -gt 0 ]; do
         if [[ "$1" != *=* ]]; then shift; fi
         DISABLE_CACHE="${1#*=}"
         ;;
+    --disable_batching*)
+        if [[ "$1" != *=* ]]; then shift; fi
+        DISABLE_BATCHING="${1#*=}"
+        ;;
     --help | -h)
         printf -- "--app <appname> one of q1,q2,q3,q4,q5,q6,q7,q8\n"
         printf -- "--exp_dir <exp_dir> required\n"
@@ -81,6 +86,7 @@ while [ $# -gt 0 ]; do
         printf -- "--flushms <flush interval in ms>\n"
         printf -- "--gua <guarantee; alo or eo>\n"
 	printf -- "--disable_cache <true or false>\n"
+	printf -- "--disable_batching <true or false>\n"
         exit 0
         ;;
     *)
@@ -143,10 +149,22 @@ if [[ "$DISABLE_CACHE" = "true" ]]; then
 	CACHE_ARG="--disable_cache"
 fi
 
+BATCH_ARG=""
+if [[ "$DISABLE_BATCHING" = "true" ]]; then
+	BATCH_ARG="--disable_batching"
+fi
+
+SRC_BATCH_ARG=""
+if [[ "$DISABLE_BATCHING" = "true" ]]; then
+	SRC_BATCH_ARG="-disableBatching=true"
+fi
+
+
 echo "app: $NAME, exp_dir: $EXP_DIR, num_instance: $NUM_INSTANCE, num events: $NUM_EVENTS, \
 	num_src: $NUM_SRC_INSTANCE, serde: $SERDE, duration: $DURATION, tps: $TPS, \
 	warm duration: $WARM_DURATION, flushMs: $FLUSH_MS, guarantee: $GUARANTEE, \
-	disable_cache: ${DISABLE_CACHE}, cache_arg: ${CACHE_ARG}, src_flush_ms: ${SRC_FLUSH_MS}"
+	disable_cache: ${DISABLE_CACHE}, cache_arg: ${CACHE_ARG}, src_flush_ms: ${SRC_FLUSH_MS}, \
+        disable_batching: ${DISABLE_BATCHING}, batch_arg: ${BATCH_ARG}, src_batch_arg: ${SRC_BATCH_ARG}"
 
 BASE_DIR=$(realpath $(dirname $0))
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -186,6 +204,9 @@ for HOST in ${ALL_BROKER_HOSTS[@]}; do
     ssh -q $HOST -oStrictHostKeyChecking=no -- sudo rm -rf /mnt/storage/kdata
     ssh -q $HOST -oStrictHostKeyChecking=no -- sudo mkdir -p /mnt/storage/kdata
     ssh -q $HOST -oStrictHostKeyChecking=no -- sudo chown ubuntu:ubuntu /mnt/storage/kdata
+    ssh -q $HOST -oStrictHostKeyChecking=no -- sudo sysctl -w vm.max_map_count=262144
+    ssh -q $HOST -- echo "ubuntu hard nofile 500000" | sudo tee -a /etc/security/limits.conf
+    ssh -q $HOST -- echo "ubuntu soft nofile 500000" | sudo tee -a /etc/security/limits.conf
 done
 
 ssh -q $MANAGER_HOST -oStrictHostKeyChecking=no -- docker network rm kstreams-test_default || true
@@ -227,28 +248,29 @@ for ((j=0; j<$NUM_SRC_INSTANCE; j++)); do
         --replicas-max-per-node=1 --hostname=source-${j} --publish mode=host,published=$PORT,target=$PORT \
         --env IID=$j ubuntu:focal /src/bin/nexmark_genevents_kafka \
         -broker $FIRST_BROKER_CONTAINER_IP:9092 -duration ${DURATION} -npar 4 -serde $SERDE \
-        -srcIns $NUM_SRC_INSTANCE -events_num $NUM_EVENTS -tps $TPS -port $PORT -flushms $SRC_FLUSH_MS" &
+        -srcIns $NUM_SRC_INSTANCE -events_num $NUM_EVENTS -tps $TPS \
+        -port $PORT -flushms $SRC_FLUSH_MS ${SRC_BATCH_ARG}" &
 done
 
 for ((k=0; k<$NUM_INSTANCE; k++)); do
     PORT=$(expr 7000 + $k)
     ssh -q $MANAGER_HOST -oStrictHostKeyChecking=no -- "docker service create \
         --mount type=bind,source=$WORKSPACE_DIR/nexmark/nexmark-kafka-streams,destination=/src \
-        --mount type=bind,source=$WORKSPACE_DIR/sharedlog-stream-experiments/nexmark_kafka-streams/shared-assets/jmx-exporter,destination=/usr/share/jmx-exporter \
+        --mount type=bind,source=$WORKSPACE_DIR/sharedlog-stream-experiments/nexmark_kafka-streams_metrics/shared-assets/jmx-exporter,destination=/usr/share/jmx-exporter \
         --constraint node.labels.app_node==true \
         --env BOOTSTRAP_SERVER_CONFIG=$FIRST_BROKER_CONTAINER_IP:9092 \
         --network kstreams-test_default --restart-condition none --replicas=1 \
         --replicas-max-per-node=1 --hostname=nexmark-${k} \
         --name kstreams-test_nexmark-${k} --publish mode=host,published=$PORT,target=$PORT \
         --publish mode=host,published=12345,target=12345 \
-        openjdk:11.0.12-jre-slim-buster \
+        azul/zulu-openjdk:11.0.17 \
         bash -c 'java -javaagent:/usr/share/jmx-exporter/jmx_prometheus_javaagent-0.16.1.jar=12345:/usr/share/jmx-exporter/kafka_streams.yml \
         -cp /src/build/libs/nexmark-kafka-streams-0.2-SNAPSHOT-uber.jar \
         com.github.nexmark.kafka.queries.RunQuery \
         --name $NAME --serde $SERDE --conf  /src/workload_config/${NAME}.properties \
         --duration $DURATION --port $PORT  \
         --flushms ${FLUSH_MS} --warmup_time ${WARM_DURATION} --guarantee ${GUARANTEE} \
-        --stats_dir /tmp/stats ${CACHE_ARG}'" &
+        --stats_dir /tmp/stats ${CACHE_ARG} ${BATCH_ARG}'" &
 done
 
 sleep 20
@@ -256,17 +278,17 @@ sleep 20
 ALL_SOURCE_HOSTS=$($HELPER_SCRIPT get-machine-with-label --machine-label=source_node)
 ALL_APP_HOSTS=$($HELPER_SCRIPT get-machine-with-label --machine-label=app_node)
 
-# for HOST in $ALL_BROKER_HOSTS; do
-# 	ssh -q $HOST -oStrictHostKeyChecking=no -- sar -o /home/ubuntu/sar_st 1 >/dev/null 2>&1 &
-# done
-# 
-# for HOST in $ALL_SOURCE_HOSTS; do
-# 	ssh -q $HOST -oStrictHostKeyChecking=no -- sar -o /home/ubuntu/sar_st 1 >/dev/null 2>&1 &
-# done
-# 
-# for HOST in $ALL_APP_HOSTS; do
-# 	ssh -q $HOST -oStrictHostKeyChecking=no -- sar -o /home/ubuntu/sar_st 1 >/dev/null 2>&1 &
-# done
+for HOST in $ALL_BROKER_HOSTS; do
+	ssh -q $HOST -oStrictHostKeyChecking=no -- sar -o /home/ubuntu/sar_st 1 >/dev/null 2>&1 &
+done
+
+for HOST in $ALL_SOURCE_HOSTS; do
+	ssh -q $HOST -oStrictHostKeyChecking=no -- sar -o /home/ubuntu/sar_st 1 >/dev/null 2>&1 &
+done
+
+for HOST in $ALL_APP_HOSTS; do
+	ssh -q $HOST -oStrictHostKeyChecking=no -- sar -o /home/ubuntu/sar_st 1 >/dev/null 2>&1 &
+done
 
 pids=()
 i=0
@@ -292,26 +314,26 @@ for pid in ${pids[*]}; do
     wait $pid
 done
 
-# for HOST in $ALL_BROKER_HOSTS; do
-# 	ssh -q $HOST -oStrictHostKeyChecking=no -- pkill sar
-# 	mkdir -p $EXP_DIR/broker_sar/$HOST
-# 	scp $HOST:/home/ubuntu/sar_st $EXP_DIR/broker_sar/$HOST
-# 	ssh -q $HOST -oStrictHostKeyChecking=no -- rm /home/ubuntu/sar_st
-# done
-# 
-# for HOST in $ALL_SOURCE_HOSTS; do
-# 	ssh -q $HOST -oStrictHostKeyChecking=no -- pkill sar
-# 	mkdir -p $EXP_DIR/source_sar/$HOST
-# 	scp $HOST:/home/ubuntu/sar_st $EXP_DIR/source_sar/$HOST
-# 	ssh -q $HOST -oStrictHostKeyChecking=no -- rm /home/ubuntu/sar_st
-# done
-# 
-# for HOST in $ALL_APP_HOSTS; do
-# 	ssh -q $HOST -oStrictHostKeyChecking=no -- pkill sar
-# 	mkdir -p $EXP_DIR/app_sar/$HOST
-# 	scp $HOST:/home/ubuntu/sar_st $EXP_DIR/app_sar/$HOST
-# 	ssh -q $HOST -oStrictHostKeyChecking=no -- rm /home/ubuntu/sar_st
-# done
+for HOST in $ALL_BROKER_HOSTS; do
+	ssh -q $HOST -oStrictHostKeyChecking=no -- pkill sar
+	mkdir -p $EXP_DIR/broker_sar/$HOST
+	scp $HOST:/home/ubuntu/sar_st $EXP_DIR/broker_sar/$HOST
+	ssh -q $HOST -oStrictHostKeyChecking=no -- rm /home/ubuntu/sar_st
+done
+
+for HOST in $ALL_SOURCE_HOSTS; do
+	ssh -q $HOST -oStrictHostKeyChecking=no -- pkill sar
+	mkdir -p $EXP_DIR/source_sar/$HOST
+	scp $HOST:/home/ubuntu/sar_st $EXP_DIR/source_sar/$HOST
+	ssh -q $HOST -oStrictHostKeyChecking=no -- rm /home/ubuntu/sar_st
+done
+
+for HOST in $ALL_APP_HOSTS; do
+	ssh -q $HOST -oStrictHostKeyChecking=no -- pkill sar
+	mkdir -p $EXP_DIR/app_sar/$HOST
+	scp $HOST:/home/ubuntu/sar_st $EXP_DIR/app_sar/$HOST
+	ssh -q $HOST -oStrictHostKeyChecking=no -- rm /home/ubuntu/sar_st
+done
 
 ssh -q $PROMETHEUS_HOST -oStrictHostKeyChecking=no -- curl -XPOST http://localhost:9090/api/v1/admin/tsdb/snapshot
 scp -r $PROMETHEUS_HOST:/mnt/storage/prometheus/snapshots $EXP_DIR/snapshots
