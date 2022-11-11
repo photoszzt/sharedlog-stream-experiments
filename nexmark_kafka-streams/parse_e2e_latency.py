0e2eb02284f8e8f9c67e16666f29d20f853c05bf
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-11-11 14:03:57 -0600

    backup script fix
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_kafka-streams/parse_e2e_latency.py
+++ nexmark_kafka-streams/parse_e2e_latency.py
@@ -5,17 +5,6 @@ import json
 import numpy as np
 import pickle
 
-stages = {
-    "q1": "query1",
-    "q2": "query2",
-    "q3": "q3JoinTable",
-    "q4": "q4Avg",
-    "q5": "q5maxbid",
-    "q6": "q6Avg",
-    "q7": "q7JoinMaxBid",
-    "q8": "q8JoinStream",
-}
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -28,33 +17,27 @@ def main():
     all_stats = {}
     for root, dirs, _ in os.walk(args.dir):
         for d in dirs:
-            if "epoch" in d:
+            if "eo" in d:
                 tps_per_work = int(d.split("_")[0][:-3])
                 if tps_per_work not in dirs_dict:
                     dirs_dict[tps_per_work] = []
                 dirs_dict[tps_per_work].append(os.path.join(root, d))
     print(dirs_dict)
     os.makedirs(args.out_stats, exist_ok=True)
-    prefix = stages[args.app]
     for tps, dirpaths in dirs_dict.items():
         all_data = []
         for dpath in dirpaths:
             e2e_latency = []
-            stats_dir = os.path.join(dpath, "stats")
+            stats_dir = os.path.join(dpath, "logs")
             for root, dirs, files in os.walk(stats_dir):
                 for fname in files:
-                    if "gz" in fname and prefix in fname:
+                    if "sink_ets" in fname:
                         fpath = os.path.join(root, fname)
-                        with gzip.open(fpath, 'rb') as f:
-                            st = json.load(f)
-                            found = False
-                            for key, value in st['Latencies'].items():
-                                if 'eventTimeLatency' in key:
-                                    if st['Latencies'][key] is not None:
-                                        e2e_latency.append(st['Latencies'][key])
-                                        found = True
-                            if not found:
-                                print(f"{fpath} event time latency is empty")
+                        with open(fpath, 'r') as f:
+                            for line in f:
+                                l = line.strip("[]\n").split(", ")
+                                l = [int(i) for i in l]
+                                e2e_latency.append(l)
             if e2e_latency:
                 e2e_lat = np.concatenate(e2e_latency)
                 p50 = np.quantile(e2e_lat, 0.5)
@@ -68,17 +51,14 @@ def main():
                 print(f"{dpath} p50: {p50} ms, p99: {p99} ms")
                 all_data.append(e2e_lat)
         print()
-        if all_data:
-            all_data_cat = np.concatenate(all_data)
-            all_p50 = np.quantile(all_data_cat, 0.5)
-            all_p99 = np.quantile(all_data_cat, 0.99)
-            print(f"{tps} p50: {all_p50} ms, p99: {all_p99} ms")
-            if tps not in all_stats:
-                all_stats[tps] = {}
-            all_stats[tps]["p50"] = p50
-            all_stats[tps]["p99"] = p99
-        else:
-            print(f"{tps} doesn't have data")
+        all_data_cat = np.concatenate(all_data)
+        all_p50 = np.quantile(all_data_cat, 0.5)
+        all_p99 = np.quantile(all_data_cat, 0.99)
+        print(f"{tps} p50: {all_p50} ms, p99: {all_p99} ms")
+        if tps not in all_stats:
+            all_stats[tps] = {}
+        all_stats[tps]["p50"] = p50
+        all_stats[tps]["p99"] = p99
     mtime = int(os.stat(args.dir).st_mtime)
     all_stats_path = os.path.join(args.out_stats, f"{args.app}_{mtime}.pickle")
     splitlat_stats_path = os.path.join(args.out_stats, f"{args.app}_split_{mtime}.pickle")
