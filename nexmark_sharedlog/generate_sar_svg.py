c27bdd278615249b81152f818a60ba2fc035edb9
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-11-15 15:28:15 -0600

    backup parsing script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ nexmark_sharedlog/generate_sar_svg.py
@@ -0,0 +1,107 @@
+import argparse
+import os
+from pathlib import Path
+import subprocess as sp
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Parse stage time')
+    parser.add_argument('--dir', type=str, help='dir to parse', required=True)
+    parser.add_argument('--out_dir', type=str, help='output_dir', required=True)
+    args = parser.parse_args()
+    dirs_dict = {}
+
+    for root, dirs, _ in os.walk(args.dir):
+        for d in dirs:
+            if "epoch" in d:
+                tps_per_work = int(d.split("_")[0][:-3])
+                if tps_per_work not in dirs_dict:
+                    dirs_dict[tps_per_work] = []
+                dirs_dict[tps_per_work].append(os.path.join(root, d))
+    for tps_per_work, dirpaths in dirs_dict.items():
+        for dirpath in dirpaths:
+            logs_path = os.path.join(dirpath, "logs")
+            source_paths = set()
+            for source_file in Path(logs_path).glob("**/source*.stderr"):
+                size = os.stat(source_file).st_size
+                if size > 4100:
+                    d = os.path.dirname(os.path.dirname(source_file))
+                    d = os.path.basename(os.path.dirname(d))
+                    source_paths.add(d)
+
+            storage_sar = os.path.join(dirpath, "storage_sar")
+            engine_sar = os.path.join(dirpath, "engine_sar")
+            pdir = os.path.basename(os.path.dirname(dirpath))
+            out_dir = os.path.join(args.out_dir, pdir)
+            out_storage_dir = os.path.join(out_dir, "storage")
+            out_engine_dir = os.path.join(out_dir, "engine")
+            os.makedirs(out_storage_dir, exist_ok=True)
+            os.makedirs(out_engine_dir, exist_ok=True)
+
+            for dirname in Path(storage_sar).glob("*.compute.internal"):
+                base_dirname = os.path.basename(dirname)
+                parent_dir = os.path.dirname(dirname)
+                dev_info = os.path.join(parent_dir, f"{base_dirname}_dev")
+                dev_name = ""
+                with open(dev_info, "r") as f:
+                    for line in f:
+                        if "nvme1n1" in line:
+                            l = line.strip().split(" ") 
+                            major = l[4].strip(",")
+                            minor = l[5]
+                            dev_name = f"dev{major}-{minor}"
+                if dev_name == "":
+                    raise Exception("doesn't find nvme1n1 in _dev file")
+
+                tps_storage_out = os.path.join(out_storage_dir, str(tps_per_work))
+                os.makedirs(tps_storage_out, exist_ok=True)
+
+                cpu_cmd = ["sadf", "-g", os.path.join(dirname, "sar_st"), 
+                    "--", "-P", "ALL", ">",
+                    os.path.join(tps_storage_out, f"{base_dirname}_storage_cpu.svg")]
+                print(" ".join(cpu_cmd))
+
+
+                disk_cmd = ["sadf", "-g", os.path.join(dirname, "sar_st"), 
+                    "--", "-d", f"--dev={dev_name}", ">",
+                    os.path.join(tps_storage_out, f"{base_dirname}_storage_disk.svg")]
+                print(" ".join(disk_cmd))
+                # sp.run(disk_cmd, shell=True)
+                net_cmd = ["sadf", "-g", os.path.join(dirname, "sar_st"), 
+                    "--", "-n", "DEV", "--iface=ens5", ">",
+                    os.path.join(tps_storage_out, f"{base_dirname}_storage_net.svg")]
+                print(" ".join(net_cmd))
+                # sp.run(net_cmd, shell=True)
+
+                mem_cmd = ["sadf", "-g", os.path.join(dirname, "sar_st"), 
+                    "--", "-r", ">",
+                    os.path.join(tps_storage_out, f"{base_dirname}_storage_mem.svg")]
+                print(" ".join(mem_cmd))
+
+            for dirname in Path(engine_sar).glob("*.compute.internal"):
+                base_dirname = os.path.basename(dirname)
+                if base_dirname in source_paths:
+                    name = "sourcegen"
+                else:
+                    name = "app"
+                tps_engine_out = os.path.join(out_engine_dir, str(tps_per_work))
+                os.makedirs(tps_engine_out, exist_ok=True)
+
+                cpu_cmd = ["sadf", "-g", os.path.join(dirname, "sar_st"), 
+                    "--", "-P", "ALL", ">",
+                    os.path.join(tps_engine_out, f"{base_dirname}_{name}_cpu.svg")]
+                print(" ".join(cpu_cmd))
+
+                net_cmd = ["sadf", "-g", os.path.join(dirname, "sar_st"), 
+                    "--", "-n", "DEV", "--iface=ens5", ">",
+                    os.path.join(tps_engine_out, f"{base_dirname}_{name}_net.svg")]
+                print(" ".join(net_cmd))
+
+                mem_cmd = ["sadf", "-g", os.path.join(dirname, "sar_st"), 
+                    "--", "-r", ">",
+                    os.path.join(tps_engine_out, f"{base_dirname}_{name}_mem.svg")]
+                print(" ".join(mem_cmd))
+
+
+if __name__ == '__main__':
+    main()
