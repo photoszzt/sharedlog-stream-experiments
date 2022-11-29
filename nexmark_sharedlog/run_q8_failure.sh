35d93f0ed57902e2f096e56627337f0299220e5c
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-11-29 13:52:00 -0600

    update failure script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/run_q8_failure.sh
+++ nexmark_sharedlog/run_q8_failure.sh
@@ -10,31 +10,29 @@ TPS_PER_WORKER=(20000 24000 28000)
 NUM_WORKER=(4)
 DURATION=330
 WARM_DURATION=0
-APP=(q8)
-DIR=(q8_boki/mem)
+APP=q8
+DIR=q8_boki/mem
 FLUSH_MS=100
 SRC_FLUSH_MS=100
 SNAPSHOT_S=0
 
-for ((i = 0; i < 3; ++i)); do
-    for ((k = 0; k < ${#APP[@]}; ++k)); do
-        cd ${DIR[k]}
-        for ((idx = 0; idx < ${#TPS_PER_WORKER[@]}; ++idx)); do
-            for ((w = 0; w < ${#NUM_WORKER[@]}; ++w)); do
-                TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_WORKER[w]})
-                EVENTS=$(expr $TPS \* $DURATION)
-                echo ${APP[k]}, ${DIR[k]}, ${EVENTS} events, ${TPS} tps
-                subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms
-                # $WORKSPACE_DIR/research-helper-scripts/microservice_helper start-machines --use-spot-instances
-                ./run_once.sh --app ${APP[k]} --exp_dir ./${NUM_WORKER[w]}src_nosnap_300_gm1/$subdir/$i/${TPS_PER_WORKER[idx]}tps_epoch/ \
-                    --gua epoch --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
-                    --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS \
-                    --src_flushms $SRC_FLUSH_MS --fail true --snapshot_s ${SNAPSHOT_S}
-                # $WORKSPACE_DIR/research-helper-scripts/microservice_helper stop-machines
-            done
+for ((i = 0; i < 1; ++i)); do
+    cd ${DIR}
+    for ((idx = 0; idx < ${#TPS_PER_WORKER[@]}; ++idx)); do
+        for ((w = 0; w < ${#NUM_WORKER[@]}; ++w)); do
+            TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_WORKER[w]})
+            EVENTS=$(expr $TPS \* $DURATION)
+            echo ${APP[k]}, ${DIR[k]}, ${EVENTS} events, ${TPS} tps
+            subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms
+
+            ./run_once.sh --app ${APP[k]} \
+                --exp_dir ./${NUM_WORKER[w]}src_nosnap_300_gm2/$subdir/$i/${TPS_PER_WORKER[idx]}tps_epoch/ \
+                --gua epoch --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
+                --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS \
+                --src_flushms $SRC_FLUSH_MS --fail true --snapshot_s ${SNAPSHOT_S}
         done
-        cd -
     done
+    cd -
 done
 
 cd q8_boki/mem
--- nexmark_sharedlog/run_q8_failure_snapshot.sh
+++ nexmark_sharedlog/run_q8_failure_snapshot.sh
@@ -10,29 +10,28 @@ TPS_PER_WORKER=(20000 24000 28000)
 NUM_WORKER=(4)
 DURATION=330
 WARM_DURATION=0
-APP=(q8)
-DIR=(q8_boki/mem)
+APP=q8
+DIR=q8_boki/mem
 FLUSH_MS=100
 SRC_FLUSH_MS=100
 SNAPSHOT_S=10
 
-for ((i = 0; i < 4; ++i)); do
-    for ((k = 0; k < ${#APP[@]}; ++k)); do
-        cd ${DIR[k]}
-        for ((idx = 0; idx < ${#TPS_PER_WORKER[@]}; ++idx)); do
-            for ((w = 0; w < ${#NUM_WORKER[@]}; ++w)); do
-                TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_WORKER[w]})
-                EVENTS=$(expr $TPS \* $DURATION)
-                echo ${APP[k]}, ${DIR[k]}, ${EVENTS} events, ${TPS} tps
-                subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms
-                ./run_once.sh --app ${APP[k]} --exp_dir ./${NUM_WORKER[w]}src_snapshot_300_gm1/$subdir/$i/${TPS_PER_WORKER[idx]}tps_epoch/ \
-                    --gua epoch --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
-                    --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS \
-                    --src_flushms $SRC_FLUSH_MS --fail true --snapshot_s ${SNAPSHOT_S}
-            done
+for ((i = 0; i < 1; ++i)); do
+    cd ${DIR}
+    for ((idx = 0; idx < ${#TPS_PER_WORKER[@]}; ++idx)); do
+        for ((w = 0; w < ${#NUM_WORKER[@]}; ++w)); do
+            TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_WORKER[w]})
+            EVENTS=$(expr $TPS \* $DURATION)
+            echo ${APP[k]}, ${DIR[k]}, ${EVENTS} events, ${TPS} tps
+            subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms
+            ./run_once.sh --app ${APP[k]} \
+                --exp_dir ./${NUM_WORKER[w]}src_snapshot_300_gm2/$subdir/$i/${TPS_PER_WORKER[idx]}tps_epoch/ \
+                --gua epoch --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
+                --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS \
+                --src_flushms $SRC_FLUSH_MS --fail true --snapshot_s ${SNAPSHOT_S}
         done
-        cd -
     done
+    cd -
 done
 
 cd q8_boki/mem
