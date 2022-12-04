86e10bc68dda99a1895f1c29deb84073238ab0c7
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-12-04 00:56:11 -0600

    update script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_kafka-streams/overall_stats.py
+++ nexmark_kafka-streams/overall_stats.py
@@ -23,28 +23,37 @@ stages = {
 }
 
 out_stage_names = {
-    "q3": {"subG1", "aucQueueDelay", "perQueueDelay",
-           "q3_sink_ets-7_proc"},
-    "q4": {"subG1", "aucQueueDelay", "bidQueueDelay",
-           "aucBidsQueueDelay", "maxBidsQueueDelay", "subG2_proc", "subG3_proc", 
-           "q4_sink_ets-7_proc",},
-    "q5": {"subG1ProcLat", "subG2ProcLat", "bidsQueueDelay", "auctionBidsQueueDelay", 
-           "q5_sink_ets-7_proc"},
+    "q3": {"subG1":"subG1(ns)", "aucQueueDelay":"auc_queue(ms)", "perQueueDelay":"per_queue(ms)",
+        "q3_sink_ets-7_proc":"subG2(ns)"},
+    "q4": {"subG1":"subG1(ns)", "aucQueueDelay":"auc_queue(ms)", "bidQueueDelay":"per_queue(ms)",
+        "aucBidsQueueDelay":"aucBid_queue(ms)", "maxBidsQueueDelay":"maxBid_queue(ms)", 
+        "subG2_proc":"subG2(ns)", "subG3_proc":"subG3(ns)", 
+        "q4_sink_ets-7_proc":"subG4(ns)",},
+    "q5": {"subG1ProcLat": "subG1(ns)", "subG2ProcLat": "subG2(ns)", 
+        "bidsQueueDelay": "bid_queue(ms)", 
+        "auctionBidsQueueDelay": "auc_queue(ms)", 
+        "q5_sink_ets-7_proc": "subG3(ns)"},
     "q6": {"subG1": "subG1(ns)", "aucQueueTime": "aucQueueTime(ms)", 
-           "bidQueueTime": "bidQueueTime(ms)", "topo2_proc": "subG2(ns)",
-           "topo3_proc": "subG3(ns)", "aucBidsQueueTime": "aucBidsQueueTime(ms)", 
-           "q6_sink_ets-7_proc": "subG4(ns)"},
+        "bidQueueTime": "bidQueueTime(ms)", "topo2_proc": "subG2(ns)",
+        "topo3_proc": "subG3(ns)", "aucBidsQueueTime": "aucBidsQueueTime(ms)", 
+        "q6_sink_ets-7_proc": "subG4(ns)"},
     "q7": {"bids_by_win_proc": "bids_by_win_proc(ns)", "bids_by_price_proc": "bids_by_price_proc(ns)", 
-           "topo2_proc": "subG2(ns)", "q7_sink_ets-7_proc": "subG3(ns)", 
-           "bids_by_win_queue": "bids_by_win_queue(ms)", 
-           "bids_by_price_queue": "bids_by_price_queue(ms)", 
-           "max_bids_queue": "max_bids_queue(ms)", },
+        "topo2_proc": "subG2(ns)", "q7_sink_ets-7_proc": "subG3(ns)", 
+        "bids_by_win_queue": "bids_by_win_queue(ms)", 
+        "bids_by_price_queue": "bids_by_price_queue(ms)", 
+        "max_bids_queue": "max_bids_queue(ms)", },
     "q8": {"subG1": "subG1(ns)", "auc_queue": "auc_queue(ms)", "per_queue": "per_queue(ms)", 
-           "subG2": "subG2(ns)",
-           "txn-begin": "txn-begin(ns)", "txn-send-offsets": "txn-send-offsets(ns)", 
-           "txn-commit": "txn-commit(ns)", "flush": "flush(ns)", 
-           "commitLat": "commitLat(ns)", "avgCommitLat": "avgCommitLat(ms)",
-           "execIntrMs": "execIntr(ms)"},
+        "subG2": "subG2(ns)",
+        "txn-begin": "txn-begin(ns)", "txn-send-offsets": "txn-send-offsets(ns)", 
+        "txn-commit": "txn-commit(ns)", "flush": "flush(ns)", 
+        "commitLat": "commitLat(ns)", "avgCommitLat": "avgCommitLat(ms)",
+        "execIntrMs": "execIntr(ms)"},
+}
+
+need_convert_ns_to_ms = {
+    "q8": {"txn-begin", "txn-send-offsets", 
+           "txn-commit", "flush", 
+           "commitLat"},
 }
 
 throughput = {
@@ -53,8 +62,9 @@ throughput = {
     "q5": [24000, 32000, 40000],
     "q6": [500, 750],
     "q7": [12000, 16000, 28000, 32000, 36000],
-    "q8": [12000, 16000, 20000, 28000, 32000],
+    #"q8": [12000, 16000, 20000, 28000, 32000],
     #"q8": [200, 1000, 4000],
+    "q8": [20000],
 }
 
 
@@ -65,23 +75,51 @@ def main():
     parser.add_argument("--out_stats", type=str, help="output stats dir", required=True)
     args = parser.parse_args()
     tp_stats = {}
+    tp_stats_summary = {}
     for tp in throughput[args.app]:
         all_stats = {}
+        stat_summary = {}
         for fname in Path(args.dir).glob(f"{tp}_*.pickle"):
             with open(fname, "rb") as f:
                 stats = pickle.load(f)
-                print(stats.keys())
                 s = stages[args.app]
-                for st in s:
-                    if st in stats:
-                        if st not in all_stats:
-                            all_stats[st] = []
-                        all_stats[st].extend(stats[st])
+                for st_name in s:
+                    if st_name in stats:
+                        if st_name not in all_stats:
+                            all_stats[st_name] = []
+                        one_stat = np.concatenate(stats[st_name])
+                        stp50 = np.quantile(one_stat, 0.5)
+                        stp99 = np.quantile(one_stat, 0.99)
+                        if st_name in need_convert_ns_to_ms[args.app]:
+                            stp50 = stp50 / 1000000.0
+                            stp99 = stp99 / 1000000.0
+                        if st_name not in stat_summary:
+                            stat_summary[st_name] = {}
+                            stat_summary[st_name]["p50"] = []
+                            stat_summary[st_name]["p99"] = []
+                            stat_summary[st_name]["files"] = []
+                        stat_summary[st_name]["files"].append(fname)
+                        stat_summary[st_name]["p50"].append(stp50)
+                        stat_summary[st_name]["p99"].append(stp99)
+                        all_stats[st_name].append(one_stat)
+        print()
+        tp_stats_summary[tp] = stat_summary
         for st in stages[args.app]:
             if st in all_stats:
                 data_arr = np.concatenate(all_stats[st])
                 all_stats[st] = data_arr
         tp_stats[tp] = all_stats
+
+    for st in stages[args.app]:
+        for tp in throughput[args.app]:
+            stats_summary = tp_stats_summary[tp]
+            p50s = stats_summary[st]["p50"]
+            p99s = stats_summary[st]["p99"]
+            files = stats_summary[st]["files"]
+            for i in range(len(p50s)):
+                print(f"{tp},{files[i]},{st},{p50s[i]},{p99s[i]}")
+        print()
+    print()
 
     summary_stats = {}
     for st in stages[args.app]:
