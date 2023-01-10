0e43457eefd0485ffb0e3f93615dcd92a120cf07
Zhiting Zhu <zhitingz@cs.utexas.edu>
2023-01-09 21:43:08 -0600

    backup script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/parse_container_stats.py
+++ nexmark_sharedlog/parse_container_stats.py
@@ -3,11 +3,23 @@ import os
 import json
 from statistics import mean
 
+stages = {
+        "q5": {"bid-keyed-by-auction", "max-bid", "auc-bid", "faas-engine"},
+        "q7": {"q7BidByPrice", "q7BidByWin", "q7JoinMaxBid", "q7MaxBid", "faas-engine"}
+}
+
+def get_name(name, stages):
+    for st in stages:
+        if st in name:
+            return st
+
 
 def main():
     parser = argparse.ArgumentParser(description='Parse stage time')
     parser.add_argument('--dir', type=str, help='dir to parse', required=True)
+    parser.add_argument('--app', type=str, help='app', required=True)
     args = parser.parse_args()
+    max_cpu = {}
     for root, dirs, files in os.walk(args.dir):
         for fname in files:
             stats = {}
@@ -40,12 +52,29 @@ def main():
                     stats[name]['memUsg'] = []
                 stats[name]['cpuPer'].append(cpu_per)
                 stats[name]['memUsg'].append(memUsg)
-            print(fname)
+            shouldPrint = True
             for n in stats:
-                print(f"{n},max cpu util: {max(stats[n]['cpuPer'])},mean cpu util: {mean(stats[n]['cpuPer'])}"
-                      f"max mem usage(MB): {max(stats[n]['memUsg'])/1024.0/1024.0},"
-                      f"mean mem usage(MB): {mean(stats[n]['memUsg'])/1024.0/1024.0}")
-            print()
+                if "nexmark-source" in n:
+                    maxCpu = max(stats[n]['cpuPer'])
+                    if maxCpu != 0:
+                        shouldPrint = False
+            st = stages[args.app]
+            if shouldPrint:
+                print(fname)
+                for n in stats:
+                    name = get_name(n, st)
+                    if name not in max_cpu and name is not None:
+                        max_cpu[name] = 0
+                    maxCpu = max(stats[n]['cpuPer'])
+                    meanCpu = mean(stats[n]['cpuPer'])
+                    if name is not None and maxCpu > max_cpu[name]:
+                        max_cpu[name] = maxCpu
+                    maxMem = max(stats[n]['memUsg'])/1024.0/1024.0
+                    meanMem = mean(stats[n]['memUsg'])/1024.0/1024.0
+                    print(f"{n}, max cpu util: {maxCpu}, max mem usage(MB): {maxMem}, "
+                          f"mean cpu util: {meanCpu}, mean mem usage(MB): {meanMem}")
+                print()
+        print(max_cpu)
 
 
 if __name__ == '__main__':
