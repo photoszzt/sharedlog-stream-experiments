46af3922aa334ba27b1e22be7410916645127def
Zhiting Zhu <zhitingz@cs.utexas.edu>
2023-05-20 12:43:56 -0500

    search flush script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ nexmark_sharedlog/run_q8_searchFlush.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -x
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+WORKSPACE_DIR=$(realpath $SCRIPT_DIR/../../)
+DIR=q8_boki/mem
+
+cd $DIR
+$WORKSPACE_DIR/research-helper-scripts/microservice_helper start-machines --use-spot-instances
+cd ../..
+
+# TPS_PER_WORKER=(4000 8000 12000 16000 20000 24000 28000 32000 36000)
+# TPS_PER_WORKER=(12000 16000 20000 28000 32000)
+TPS_PER_WORKER=(12000)
+NUM_WORKER=(4)
+DURATION=180
+WARM_DURATION=0
+APP=q8
+FLUSH_MS=(10 7 5 3)
+SNAPSHOT_S=10
+# SRC_FLUSH_MS=10
+
+for ((iter=3; iter < 6; ++iter)); do
+    cd ${DIR}
+    for ((j=0; j<${#FLUSH_MS[@]}; ++j)); do
+        for ((idx = 0; idx < ${#TPS_PER_WORKER[@]}; ++idx)); do
+            for ((w = 0; w < ${#NUM_WORKER[@]}; ++w)); do
+                SRC_FLUSH_MS=${FLUSH_MS[j]}
+                TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_WORKER[w]})
+                EVENTS=$(expr $TPS \* $DURATION)
+                echo ${APP}, ${DIR}, ${EVENTS} events, ${TPS} tps
+                subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS[j]}ms_src${SRC_FLUSH_MS}ms
+
+                # ./run_once.sh --app ${APP} \
+                #     --exp_dir ./${NUM_WORKER[w]}src_generics/$subdir/$iter/${TPS_PER_WORKER[idx]}tps_alo/ \
+                #     --gua alo --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
+                #     --tps ${TPS} --warm_duration ${WARM_DURATION} \
+                #     --flushms ${FLUSH_MS[j]} --src_flushms $SRC_FLUSH_MS
+
+                ./run_once.sh --app ${APP} \
+                    --exp_dir ./${NUM_WORKER[w]}src_flushSameSrc/$subdir/$iter/${TPS_PER_WORKER[idx]}tps_epoch/ \
+                    --gua epoch --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
+                    --tps ${TPS} --warm_duration ${WARM_DURATION} \
+                    --flushms ${FLUSH_MS[j]} --src_flushms $SRC_FLUSH_MS \
+                    --snapshot_s ${SNAPSHOT_S}
+
+                # ./run_once.sh --app ${APP} \
+                #     --exp_dir ./${NUM_WORKER[w]}src_none_gm1/$subdir/$iter/${TPS_PER_WORKER[idx]}tps_epoch/ \
+                #     --gua none --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
+                #     --tps ${TPS} --warm_duration ${WARM_DURATION} \
+                #     --flushms ${FLUSH_MS[j]} --src_flushms $SRC_FLUSH_MS \
+                #     --snapshot_s 0
+
+                ./run_once.sh --app ${APP} \
+                    --exp_dir ./${NUM_WORKER[w]}src_flushSameSrc/$subdir/$iter/${TPS_PER_WORKER[idx]}tps_2pc/ \
+                    --gua 2pc --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
+                    --tps ${TPS} --warm_duration ${WARM_DURATION} \
+                    --flushms ${FLUSH_MS[j]} --src_flushms $SRC_FLUSH_MS --snapshot_s $SNAPSHOT_S
+            done
+        done
+    done
+    cd -
+done
+
+cd $DIR
+$WORKSPACE_DIR/research-helper-scripts/microservice_helper stop-machines
+cd ../..
