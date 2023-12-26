b8d2f05cfeb20431186ab1038d0afb8de6713c2f
Zhiting Zhu <zhitingz@cs.utexas.edu>
2023-12-25 22:22:25 -0600

    install deps script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ setup_machine/install_deps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -xeuo pipefail
+source /etc/lsb-release
+echo $DISTRIB_RELEASE
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+sudo apt-get update
+sudo apt-get install -y build-essential autoconf automake pkg-config libtool numactl \
+  clang-15
+
+go_tar=go1.21.5.linux-amd64.tar.gz
+wget https://go.dev/dl/${go_tar} -O $HOME/${go_tar}
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $HOME/${go_tar} || true
+echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.profile
+rm $HOME/${go_tar} || true
+
+if [ ! -d $HOME/mambaforge ]; then
+    wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
+    bash Mambaforge-Linux-x86_64.sh -b
+    rm Mambaforge-Linux-x86_64.sh
+    source $HOME/mambaforge/bin/activate && mamba init
+fi
+
+if ! which rustc; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y;
+    source ~/.cargo/env
+fi
+
+source $HOME/.cargo/env
+
+# install gotask
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.local/bin
