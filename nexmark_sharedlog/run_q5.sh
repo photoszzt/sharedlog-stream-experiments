ff0d97afcf6b032de7aac8aa25a65cd5b89eef3b
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-01-17 09:45:07 -0600

    fix bash cmd line break
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/run_q5.sh
+++ nexmark_sharedlog/run_q5.sh
@@ -17,7 +17,7 @@ APP=q5
 FLUSH_MS=100
 SRC_FLUSH_MS=100
 SNAPSHOT_S=10
-modes=(alo epoch none 2pc align_chkpt)
+modes=(epoch none 2pc align_chkpt)
 
 for ((iter=0; iter < 3; ++iter)); do
     cd ${DIR}
@@ -28,7 +28,7 @@ for ((iter=0; iter < 3; ++iter)); do
             echo ${APP}, ${DIR}, ${EVENTS} events, ${TPS} tps
             subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms
             for mode in ${modes[@]}; do
-                ./run_once.sh --app ${APP} 
+                ./run_once.sh --app ${APP} \
                     --exp_dir ./${NUM_WORKER[w]}src_1/${subdir}/${iter}/${TPS_PER_WORKER[idx]}tps_${mode}/ \
                     --gua $mode --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
                     --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS --src_flushms $SRC_FLUSH_MS \
--- nexmark_sharedlog/run_q7.sh
+++ nexmark_sharedlog/run_q7.sh
@@ -9,6 +9,7 @@ $WORKSPACE_DIR/research-helper-scripts/microservice_helper start-machines --use-
 cd ../..
 
 TPS_PER_WORKER=(4000 8000 12000 16000 20000 24000 28000 32000 36000 40000)
+# TPS_PER_WORKER=(4000 12000 20000 28000 36000 40000)
 # TPS_PER_WORKER=(48000)
 NUM_WORKER=(4)
 DURATION=180
@@ -17,7 +18,7 @@ APP=q7
 FLUSH_MS=100
 SRC_FLUSH_MS=100
 SNAPSHOT_S=10
-modes=(alo epoch none 2pc align_chkpt)
+modes=(epoch none 2pc align_chkpt)
 
 for ((iter=0; iter < 3; ++iter)); do
     cd ${DIR}
@@ -28,7 +29,7 @@ for ((iter=0; iter < 3; ++iter)); do
             echo ${APP}, ${DIR}, ${EVENTS} events, ${TPS} tps
             subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms
             for mode in ${modes[@]}; do
-                ./run_once.sh --app ${APP} 
+                ./run_once.sh --app ${APP} \
                     --exp_dir ./${NUM_WORKER[w]}src_1/${subdir}/${iter}/${TPS_PER_WORKER[idx]}tps_${mode}/ \
                     --gua $mode --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
                     --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS --src_flushms $SRC_FLUSH_MS \
