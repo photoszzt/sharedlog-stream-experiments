380036e08dfcdfaf1884bf015fc905fc076e585b
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-02-14 16:33:17 -0600

    steps to install docker
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- setup_machine/install_deps.sh
+++ setup_machine/install_deps.sh
@@ -11,7 +11,7 @@ echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/d
 sudo apt-get update
 sudo apt-get install -y build-essential autoconf automake pkg-config libtool numactl \
   clang
-sudo apt install zulu21-jdk
+sudo apt install -y zulu21-jdk
 
 go_tar=go1.21.5.linux-amd64.tar.gz
 wget https://go.dev/dl/${go_tar} -O $HOME/${go_tar}
@@ -35,3 +35,21 @@ source $HOME/.cargo/env
 
 # install gotask
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.local/bin
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+      sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo usermod -aG docker $USER
