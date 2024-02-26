542439e82931dd0c27b96f2e5fd050daa364aaf8
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-02-26 01:04:34 -0600

    rorate execute order
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_kafka-streams/run_q8.sh
+++ nexmark_kafka-streams/run_q8.sh
@@ -4,36 +4,28 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKSPACE_DIR=$(realpath $SCRIPT_DIR/../../)
 $WORKSPACE_DIR/research-helper-scripts/microservice_helper start-machines --use-spot-instances
 
-TPS_PER_WORKER=(20000)
+TPS_PER_WORKER=(16000 20000 24000)
 # TPS_PER_WORKER=(4000)
 DURATION=180
 WARM_DURATION=0
 APP=(q8)
-FLUSH_MS=(100)
+FLUSH_MS=100
 NUM_INS=4
+SRC_FLUSH_MS=${FLUSH_MS}
 
-for ((iter=0; iter<2; iter++)); do
-    for ((j=0; j<${#APP[@]}; ++j)); do
-        for ((k=0; k<${#FLUSH_MS[@]}; ++k)); do
-            for ((idx=0; idx<${#TPS_PER_WORKER[@]}; ++idx)); do
-                SRC_FLUSH_MS=${FLUSH_MS[k]}
-                TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_INS})
-                EVENTS=$(expr ${TPS} \* $DURATION)
-                    echo ${APP[j]}, ${EVENTS} events, ${TPS} tps
-                subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS[k]}ms_src${SRC_FLUSH_MS}ms
+for ((j=0; j<${#APP[@]}; ++j)); do
+    for ((idx=0; idx<${#TPS_PER_WORKER[@]}; ++idx)); do
+        TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_INS})
+        EVENTS=$(expr ${TPS} \* $DURATION)
+        echo ${APP[j]}, ${EVENTS} events, ${TPS} tps
+        subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms
 
-                # ./nexmark.sh --app ${APP[j]} \
-                #     --exp_dir ./${APP[j]}/4src_ets/$subdir/${TPS_PER_WORKER[idx]}tps_${EVENTS}_alo/ \
-                #     --nins 4 --nsrc 4 --serde msgp --duration $DURATION --nevents ${EVENTS} \
-                #     --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS \
-                #     --src_flushms ${SRC_FLUSH_MS} --gua alo
- 
-                ./nexmark.sh --app ${APP[j]} \
-                    --exp_dir ./${APP[j]}/4src_20msbackoff/$subdir/$iter/${TPS_PER_WORKER[idx]}tps_eo/ \
-                    --nins 4 --nsrc 4 --serde msgp --duration $DURATION --nevents ${EVENTS} \
-                    --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms ${FLUSH_MS[k]} \
-                    --src_flushms ${SRC_FLUSH_MS} --gua eo
-            done
+        for ((iter=0; iter<2; iter++)); do
+            ./nexmark.sh --app ${APP[j]} \
+                --exp_dir ./${APP[j]}/4src_r6id/$subdir/$iter/${TPS_PER_WORKER[idx]}tps_eo/ \
+                --nins 4 --nsrc 4 --serde msgp --duration $DURATION --nevents ${EVENTS} \
+                --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms ${FLUSH_MS} \
+                --src_flushms ${SRC_FLUSH_MS} --gua eo
         done
     done
 done
