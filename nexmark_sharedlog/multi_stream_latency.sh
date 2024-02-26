e3ca40ed4d41646a5f44468851cc1dedc0d3b43f
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-02-26 14:36:23 -0600

    parse multi stream microbench results
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ nexmark_sharedlog/multi_stream_latency.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+OUT_DIR=$SCRIPT_DIR/../pub_data/micro/multi_instances/
+mkdir -p $OUT_DIR
+instances=(8 16)
+for j in ${instances[@]}; do
+  DIR=$OUT_DIR/${j}ins/q8-180s-0swarm-100ms-src100ms
+  mkdir -p $DIR
+  for ((i=0; i < 3; i++)); do
+    latency scan --prefix q8JoinStream --suffix .json.gz --output $DIR $SCRIPT_DIR/q8_boki/mem_4node_${j}ins/${j}src_4node_${j}ins/180s_0swarm_100ms_src100ms/$i
+  done
+done
+
+for j in ${instances[@]}; do
+  mv $OUT_DIR/${j}ins/q8-180s-0swarm-100ms-src100ms/2pc* $OUT_DIR/2pc/${j}ins/
+  mv $OUT_DIR/${j}ins/q8-180s-0swarm-100ms-src100ms/eo* $OUT_DIR/impeller/${j}ins/
+  mv $OUT_DIR/${j}ins/q8-180s-0swarm-100ms-src100ms/align_chkpt* $OUT_DIR/align_chkpt/${j}ins/
+done
