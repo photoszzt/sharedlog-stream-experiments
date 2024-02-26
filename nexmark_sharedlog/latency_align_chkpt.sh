79448f1da98f5f3be479b512573c706b0837d906
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-02-26 01:01:52 -0600

    script to parse all align checkpt data
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ nexmark_sharedlog/latency_align_chkpt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+OUT_DIR=$SCRIPT_DIR/../pub_data/fig10_fig11/q38_rerun/align_chkpt/
+mkdir -p $OUT_DIR
+
+mkdir -p $OUT_DIR/q1-align_chkpt-180s-0swarm-10ms-src10ms/
+mkdir -p $OUT_DIR/q2-align_chkpt-180s-0swarm-10ms-src10ms/
+mkdir -p $OUT_DIR/q3-align_chkpt-180s-0swarm-100ms-src100ms/
+mkdir -p $OUT_DIR/q4-align_chkpt-180s-0swarm-100ms-src100ms/
+mkdir -p $OUT_DIR/q5-align_chkpt-180s-0swarm-100ms-src100ms/
+mkdir -p $OUT_DIR/q6-align_chkpt-180s-0swarm-100ms-src100ms/
+mkdir -p $OUT_DIR/q7-align_chkpt-180s-0swarm-100ms-src100ms/
+mkdir -p $OUT_DIR/q8-align_chkpt-180s-0swarm-100ms-src100ms/
+
+for ((i=0; i < 3; i++)); do
+  latency scan --prefix query1 --suffix .json.gz --output $OUT_DIR/q1-align_chkpt-180s-0swarm-10ms-src10ms/ $SCRIPT_DIR/q1_boki/4src_1/180s_0swarm_100ms_src10ms/$i
+  latency scan --prefix query2 --suffix .json.gz --output $OUT_DIR/q2-align_chkpt-180s-0swarm-10ms-src10ms/ $SCRIPT_DIR/q2_boki/4src_1/180s_0swarm_100ms_src10ms/$i
+  latency scan --prefix q3JoinTable --suffix .json.gz --output $OUT_DIR/q3-align_chkpt-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q3_boki/mem/4src_1/180s_0swarm_100ms_src100ms/$i
+  latency scan --prefix q4Avg --suffix .json.gz --output $OUT_DIR/q4-align_chkpt-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q4_boki/4src_1/180s_0swarm_100ms_src100ms/$i
+  latency scan --prefix q5maxbid --suffix .json.gz --output $OUT_DIR/q5-align_chkpt-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q5_boki/mem/4src_1/180s_0swarm_100ms_src100ms/$i
+  latency scan --prefix q6Avg --suffix .json.gz --output $OUT_DIR/q6-align_chkpt-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q6_boki/4src_1/180s_0swarm_100ms_src100ms/$i
+  latency scan --prefix q7JoinMaxBid --suffix .json.gz --output $OUT_DIR/q7-align_chkpt-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q7_boki/mem/4src_1/180s_0swarm_100ms_src100ms/$i
+  latency scan --prefix q8JoinStream --suffix .json.gz --output $OUT_DIR/q8-align_chkpt-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q8_boki/mem/4src_1/180s_0swarm_100ms_src100ms/$i
+done
