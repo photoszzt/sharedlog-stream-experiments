8db3a78706c032614e69f6530a3c41e56ffd32ae
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-07 14:52:53 -0600

    parse any stats
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/analysis/parse_stat.py
+++ nexmark_sharedlog/analysis/parse_stat.py
@@ -36,9 +36,14 @@ def gen_path(app_str):
 
 def parse_stat(fname, tps_stat, stat_id):
     p = sp.run(['rg', stat_id, fname], stdout=sp.PIPE, stderr=sp.PIPE, encoding='utf-8')
+    count = 0
     for line in p.stdout.splitlines():
         if stat_id in line:
-            l = line.strip().split(": ")[1]
+            splt = line.strip().split(": ")
+            l = splt[1]
+            name = splt[0]
+            c = int(name.split(' ')[-2].strip("("))
+            count += c
             data = l.split(", ")
             if "data" in l:
                 d = data[1].split('=')[1].strip("[]{}").split(" ")
@@ -49,19 +54,20 @@ def parse_stat(fname, tps_stat, stat_id):
                 p99 = int(data[3].split('=')[1])
                 tps_stat['p50'].append(p50)
                 tps_stat['p99'].append(p99)
+    if count != 0:
+        tps_stat['count'].append(count)
 
 
-def parse_files(dirs_dict):
+def parse_files(dirs_dict, stat_id):
     stat = {}
     for tps_per_work, dirpaths in dirs_dict.items():
         if tps_per_work not in stat:
-            stat[tps_per_work] = {'raw_data': [], 'p50': [], 'p99': []}
+            stat[tps_per_work] = {'raw_data': [], 'p50': [], 'p99': [], 'count': []}
         for dirpath in dirpaths:
             for fname in Path(dirpath).glob("**/*.stderr"):
                 basename = os.path.basename(fname)
                 stage = basename.split("_")[0]
-                if stage == 'chkptmngr':
-                    parse_stat(fname, stat[tps_per_work], "e2eChkptElapsed")
+                parse_stat(fname, stat[tps_per_work], stat_id)
     return stat
 
 
@@ -81,6 +87,7 @@ def update_per_app_dir(p, dirs_dict):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--app', type=str, help='app to parse', required=True)
+    parser.add_argument('--stat_id', type=str, help='stat name', required=True)
     args = parser.parse_args()
     
     all_app_stat = {}
@@ -96,32 +103,42 @@ def main():
     #     for tps, dirs in dirs_dict.items():
     #         print(tps, dirs)
     for app, dirs_dict in dirs_dicts.items():
-        stat = parse_files(dirs_dict)
+        stat = parse_files(dirs_dict, args.stat_id)
+        print(stat)
         for tps, st in stat.items():
             if tps not in all_app_stat[app]:
-                all_app_stat[app][tps] = {'raw_data': [], 'p50': [], 'p99': []}
+                all_app_stat[app][tps] = {'raw_data': [], 'p50': [], 'p99': [], 'count': []}
             if len(st['p50']) != 0:
                 all_app_stat[app][tps]['p50'].append(st['p50'])
                 all_app_stat[app][tps]['p99'].append(st['p99'])
             if len(st['raw_data']) != 0:
                 all_data_arr = np.concatenate(st['raw_data'])
                 all_app_stat[app][tps]['raw_data'].append(all_data_arr)
+            if len(st['count']) != 0:
+                all_app_stat[app][tps]['count'].append(st['count'])
     for app, app_st_arr in all_app_stat.items():
         all_tps = sorted(app_st_arr.keys())
         for tps in all_tps:
             st = app_st_arr[tps]
             mean_p50 = -1
             mean_p99 = -1
-            all_data_arr = np.concatenate(st['raw_data'])
+            mean_count = -1
+            p50 = -1
+            p99 = -1
+            if len(all_data_arr) != 0:
+                all_data_arr = np.concatenate(st['raw_data'])
+                p50 = np.quantile(all_data_arr, 0.5)
+                p99 = np.quantile(all_data_arr, 0.99)
             if len(st['p50']) != 0:
                 all_p50 = np.concatenate(st['p50'])
                 mean_p50 = mean(all_p50)
             if len(st['p99']) != 0:
                 all_p99 = np.concatenate(st['p99'])
                 mean_p99 = mean(all_p99)
-            p50 = np.quantile(all_data_arr, 0.5)
-            p99 = np.quantile(all_data_arr, 0.99)
-            print(f'{app}, {tps}, m_p50: {mean_p50}, m_p99: {mean_p99}, p50_rest: {p50}, p99_rest: {p99}')
+            if len(st['count']) != 0:
+                all_count = np.concatenate(st['count'])
+                mean_count = mean(all_count)
+            print(f'{app}, {tps}, m_p50: {mean_p50}, m_p99: {mean_p99}, p50_rest: {p50}, p99_rest: {p99}, mean_count: {mean_count}')
 
 
 if __name__ == '__main__':
