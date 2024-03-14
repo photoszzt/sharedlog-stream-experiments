5ae61b4ae73dd437e029bb92ebb138ba44f9b4fe
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-13 20:50:27 -0500

    only show epoch and 2pc
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pub_data/micro/plot.py
+++ pub_data/micro/plot.py
@@ -4,10 +4,10 @@ import csv
 headers = ['del', 'tps', 'trials', 'pts', 'avg', 'std', 'min', 'p25', 'p50', 'p75', 'p90', 'p99', 'max']
 
 def load(exp_dir):
-    rows = subprocess.run(["./latency", "query", exp_dir], stdout=subprocess.PIPE)
+    rows = subprocess.run(["latency", "query", exp_dir], stdout=subprocess.PIPE)
     rows = rows.stdout.decode('utf-8').strip().split('\n')
     rows = [row for row in csv.DictReader(rows, fieldnames=headers) 
-            if (row['del'] == 'eo' or row['del'] == '2pc' or row['del'] == 'align_chkpt') and int(row['tps']) in [24000]]
+            if (row['del'] == 'eo' or row['del'] == '2pc' or row['del'] == 'align_chkpt') and int(row['tps']) in [28000, 14000, 7000, 3500]]
     rows.sort(key=lambda row: int(row['tps']))
     return rows
 
@@ -15,7 +15,7 @@ def load(exp_dir):
 def get_tps(ins, m):
     tps = []
     for i in ins:
-        v = load(f"./{m}/{i}ins/q8-180s-0swarm-100ms-src100ms/")
+        v = load(f"{m}/{i}ins/q8-180s-0swarm-100ms-src100ms/")
         tps.append(v[0])
     return tps
 
@@ -23,12 +23,12 @@ colors = ['blue', 'orange', 'green', 'purple', 'olive']
 
 
 def main():
-    ins=[4, 8, 16]
+    ins=[4, 8, 16, 32]
     fig = plt.figure(figsize=(7, 3.2), layout='constrained')
     ax1 = plt.subplot(111)
-    impeller_tps = get_tps(ins, 'impeller')
-    twopc_tps = get_tps(ins, '2pc')
-    align_chkpt_tps = get_tps(ins, 'align_chkpt')
+    impeller_tps = get_tps(ins, './multi_instances_0extra_kvrocks/impeller/')
+    twopc_tps = get_tps(ins, './multi_instances_0extra_kvrocks/2pc/')
+    align_chkpt_tps = get_tps(ins, './multi_instances_0extra_kvrocks/align_chkpt/')
 
     impeller_p50 = [int(row['p50']) for row in impeller_tps]
     impeller_p99 = [int(row['p99']) for row in impeller_tps]
@@ -47,12 +47,13 @@ def main():
     l2, = ax1.plot(ins, impeller_p99, label='Impeller p99', ls='--', marker='o', color=colors[0])
     l3, = ax1.plot(ins, twopc_p50, label='2pc on Impeller p50', marker='s', color=colors[1])
     l4, = ax1.plot(ins, twopc_p99, label='2pc on Impeller p99', ls='--', marker='s', color=colors[1])
-    l5, = ax1.plot(ins, align_chkpt_p50, label='Align chkpt on Impeller p50', marker='p', color=colors[4])
-    l6, = ax1.plot(ins, align_chkpt_p99, label='Align chkpt on Impeller p99', ls='--', marker='p', color=colors[4])
-    handles = [l1, l2, l3, l4, l5, l6]
+    # l5, = ax1.plot(ins, align_chkpt_p50, label='Align chkpt on Impeller p50', marker='p', color=colors[4])
+    # l6, = ax1.plot(ins, align_chkpt_p99, label='Align chkpt on Impeller p99', ls='--', marker='p', color=colors[4])
+    # handles = [l1, l2, l3, l4, l5, l6]
+    handles = [l1, l2, l3, l4]
 
     ax1.tick_params(labelsize=16)
-    ax1.set_xlabel('Input throughput(events/s)', fontsize=16)
+    ax1.set_xlabel('number of streams', fontsize=16)
     ax1.set_ylabel('Event time latency(ms)', fontsize=16)
     fig.legend(ncol=2, handles=handles, fontsize=16, loc='upper center', bbox_to_anchor=(0.5, 1.4))
     plt.savefig('q8_4-16ins.pdf', bbox_inches='tight')
