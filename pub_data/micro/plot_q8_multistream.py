9218f589515ca6025892ee55eeb03e9680547cdf
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-13 20:50:07 -0500

    plot metrics
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pub_data/micro/plot_q8_multistream.py
+++ pub_data/micro/plot_q8_multistream.py
@@ -8,22 +8,89 @@ tps_map = {
     "32": "3500",
 }
 
+colors = ['blue', 'orange', 'green', 'purple', 'olive']
+markers = ['o', 's', 'v', '+']
+
 def main():
     with open("./q8_multistream_stats.json", "r") as f:
         stats = json.load(f)
     ins = ["4", "8", "16", "32"]
-    stages = stats["4"]['epoch']['flush_all']['per_stage'].keys()
+    metrics = ['flush_all', 'flush_at_least_one']
+    for m in metrics:
+        stages = stats["4"]['epoch'][m]['per_stage'].keys()
+        for st in stages:
+            plt.figure(figsize=(7, 3), layout="constrained")
+            epoch_fa_p50 = [stats[i]['epoch'][m]['per_stage'][st][tps_map[i]]['p50']/1000 for i in ins]
+            epoch_fa_p99 = [stats[i]['epoch'][m]['per_stage'][st][tps_map[i]]['p99']/1000 for i in ins]
+            twopc_fa_p50 = [stats[i]['2pc'][m]['per_stage'][st][tps_map[i]]['p50']/1000 for i in ins]
+            twopc_fa_p99 = [stats[i]['2pc'][m]['per_stage'][st][tps_map[i]]['p99']/1000 for i in ins]
+            print(f'{st} epoch {m} p50(ms): {epoch_fa_p50}')
+            print(f'{st} epoch {m} p99(ms): {epoch_fa_p99}')
+            print(f'{st} 2pc {m} p50(ms): {twopc_fa_p50}')
+            print(f'{st} 2pc {m} p99(ms): {twopc_fa_p99}')
+            l1, = plt.plot(ins, epoch_fa_p50, label=f'{st} epoch p50', marker=markers[0], color=colors[0])
+            l2, = plt.plot(ins, epoch_fa_p99, label=f'{st} epoch p99', marker=markers[0], ls='--', color=colors[0])
+            l3, = plt.plot(ins, twopc_fa_p50, label=f'{st} 2pc p50', marker=markers[1], color=colors[1])
+            l4, = plt.plot(ins, twopc_fa_p99, label=f'{st} 2pc p99', marker=markers[1], ls='--', color=colors[1])
+            handles = [l1, l2, l3, l4]
+            plt.xlabel("number of substreams")
+            plt.ylabel("time(ms)")
+            plt.title(f"{m}")
+            plt.legend(handles=handles, ncol=2, loc='upper center', bbox_to_anchor=(0.5, 1.4))
+            plt.savefig(f"q8_metrics/q8_{m}_{st}.pdf")
+            print()
+
+    epoch_metrics = ['progress_mark', 'mark_part', 'append_mark', 'prepare_mark']
+    for m in epoch_metrics:
+        plt.figure(figsize=(7, 3), layout="constrained")
+        stages = stats["4"]['epoch'][m]['per_stage'].keys()
+        for idx, st in enumerate(stages):
+            p50 = [stats[i]['epoch'][m]['per_stage'][st][tps_map[i]]['p50']/1000 for i in ins]
+            p99 = [stats[i]['epoch'][m]['per_stage'][st][tps_map[i]]['p99']/1000 for i in ins]
+            print(f'{st} {m} p50(ms): {p50}')
+            print(f'{st} {m} p99(ms): {p99}')
+            plt.plot(ins, p50, label=f'{st} p50', marker=markers[idx], color=colors[idx])
+            plt.plot(ins, p99, label=f'{st} p99', marker=markers[idx], color=colors[idx], ls='--')
+        plt.xlabel("number of substreams")
+        plt.ylabel("time(ms)")
+        plt.title(f"{m}")
+        plt.legend(ncol=2, loc='upper center', bbox_to_anchor=(0.5, 1.4))
+        plt.savefig(f"q8_metrics/q8_{m}.pdf")
+        print()
+
+    twopc_metrics = ['commitTxnAPITime', 'txnCommitTime', 'sendOffsetTime', 'waitPrevTxn']
+    for m in twopc_metrics:
+        plt.figure(figsize=(7, 3), layout="constrained")
+        stages = stats["4"]['2pc'][m]['per_stage'].keys()
+        for idx, st in enumerate(stages):
+            p50 = [stats[i]['2pc'][m]['per_stage'][st][tps_map[i]]['p50']/1000 for i in ins]
+            p99 = [stats[i]['2pc'][m]['per_stage'][st][tps_map[i]]['p99']/1000 for i in ins]
+            print(f'{st} {m} p50(ms): {p50}')
+            print(f'{st} {m} p99(ms): {p99}')
+            plt.plot(ins, p50, label=f'{st} p50', marker=markers[idx], color=colors[idx])
+            plt.plot(ins, p99, label=f'{st} p99', marker=markers[idx], color=colors[idx], ls='--')
+        plt.xlabel("number of substreams")
+        plt.ylabel("time(ms)")
+        plt.title(f"{m}")
+        plt.legend(ncol=2, loc='upper center', bbox_to_anchor=(0.5, 1.4))
+        plt.savefig(f"q8_metrics/q8_{m}.pdf")
+        print()
+
+    m='marker_size'
     plt.figure(figsize=(7, 3), layout="constrained")
-    for st in stages:
-        epoch_fa_p50 = [stats[i]['epoch']['flush_all']['per_stage'][st][tps_map[i]]['p50'] for i in ins]
-        epoch_fa_p99 = [stats[i]['epoch']['flush_all']['per_stage'][st][tps_map[i]]['p99'] for i in ins]
-        twopc_fa_p50 = [stats[i]['2pc']['flush_all']['per_stage'][st][tps_map[i]]['p50'] for i in ins]
-        twopc_fa_p99 = [stats[i]['2pc']['flush_all']['per_stage'][st][tps_map[i]]['p99'] for i in ins]
-        plt.plot(ins, epoch_fa_p50, label=f'{st} epoch p50')
-        plt.plot(ins, epoch_fa_p99, label=f'{st} epoch p99')
-        plt.plot(ins, twopc_fa_p50, label=f'{st} 2pc p50')
-        plt.plot(ins, twopc_fa_p99, label=f'{st} 2pc p99')
-    plt.savefig("q8_flush_all.pdf")
+    stages = stats["4"]['epoch'][m]['per_stage'].keys()
+    for idx, st in enumerate(stages):
+        p50 = [stats[i]['epoch'][m]['per_stage'][st][tps_map[i]]['p50']/1024 for i in ins]
+        p99 = [stats[i]['epoch'][m]['per_stage'][st][tps_map[i]]['p99']/1024 for i in ins]
+        print(f'{st} {m} p50(KB): {p50}')
+        print(f'{st} {m} p99(KB): {p99}')
+        plt.plot(ins, p50, label=f'{st} p50', marker=markers[idx], color=colors[idx])
+        plt.plot(ins, p99, label=f'{st} p99', marker=markers[idx], color=colors[idx], ls='--')
+    plt.xlabel("number of substreams")
+    plt.ylabel("size(KB)")
+    plt.title(f"{m}")
+    plt.legend(ncol=2, loc='upper center', bbox_to_anchor=(0.5, 1.4))
+    plt.savefig(f"q8_metrics/q8_{m}.pdf")
 
 
 if __name__ == '__main__':
