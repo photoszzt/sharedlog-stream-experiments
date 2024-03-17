e8c20a6a97d82de02f6181aeaf12fa5d4f5a523a
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-16 20:38:32 -0500

    check dir
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/latency_align_chkpt.sh
+++ nexmark_sharedlog/latency_align_chkpt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-OUT_DIR=$SCRIPT_DIR/../pub_data/fig10_fig11/q38_rerun/align_chkpt_kvrocks/
+OUT_DIR=$SCRIPT_DIR/../pub_data/fig10_fig11/q38_rerun/align_chkpt_kvrocks2/
 mkdir -p $OUT_DIR
 
 mkdir -p $OUT_DIR/q1-180s-0swarm-100ms-src10ms/
@@ -13,13 +13,29 @@ mkdir -p $OUT_DIR/q6-180s-0swarm-100ms-src100ms/
 mkdir -p $OUT_DIR/q7-180s-0swarm-100ms-src100ms/
 mkdir -p $OUT_DIR/q8-180s-0swarm-100ms-src100ms/
 
-for ((i=0; i < 3; i++)); do
-  latency scan --prefix query1 --suffix .json.gz --output $OUT_DIR/q1-180s-0swarm-10ms-src10ms/ $SCRIPT_DIR/q1_boki/4src_2/180s_0swarm_100ms_src10ms/$i || true
-  latency scan --prefix query2 --suffix .json.gz --output $OUT_DIR/q2-180s-0swarm-10ms-src10ms/ $SCRIPT_DIR/q2_boki/4src_2/180s_0swarm_100ms_src10ms/$i || true
-  latency scan --prefix q3JoinTable --suffix .json.gz --output $OUT_DIR/q3-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q3_boki/mem/4src_2/180s_0swarm_100ms_src100ms/$i/ || true
-  latency scan --prefix q4Avg --suffix .json.gz --output $OUT_DIR/q4-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q4_boki/4src_2/180s_0swarm_100ms_src100ms/$i || true
-  latency scan --prefix q5maxbid --suffix .json.gz --output $OUT_DIR/q5-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q5_boki/mem/4src_2/180s_0swarm_100ms_src100ms/$i || true
-  latency scan --prefix q6Avg --suffix .json.gz --output $OUT_DIR/q6-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q6_boki/4src_2/180s_0swarm_100ms_src100ms/$i || true
-  # # latency scan --prefix q7JoinMaxBid --suffix .json.gz --output $OUT_DIR/q7-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q7_boki/mem/4src_2/180s_0swarm_100ms_src100ms/$i
-  latency scan --prefix q8JoinStream --suffix .json.gz --output $OUT_DIR/q8-180s-0swarm-100ms-src100ms/ $SCRIPT_DIR/q8_boki/mem/4src_2/180s_0swarm_100ms_src100ms/$i || true
+for ((i=0; i < 6; i++)); do
+  echo "q1"
+  INDIR=$SCRIPT_DIR/q1_boki/4src_3/180s_0swarm_100ms_src10ms/$i
+  [[ -d $INDIR ]] && latency scan --prefix query1 --suffix .json.gz --output $OUT_DIR/q1-180s-0swarm-100ms-src10ms/ $INDIR
+  echo "q2"
+  INDIR=$SCRIPT_DIR/q2_boki/4src_3/180s_0swarm_100ms_src10ms/$i
+  [[ -d $INDIR ]] && latency scan --prefix query2 --suffix .json.gz --output $OUT_DIR/q2-180s-0swarm-100ms-src10ms/ $INDIR
+  echo "q3"
+  INDIR=$SCRIPT_DIR/q3_boki/mem/4src_3/180s_0swarm_100ms_src100ms/$i/
+  [[ -d $INDIR ]] && latency scan --prefix q3JoinTable --suffix .json.gz --output $OUT_DIR/q3-180s-0swarm-100ms-src100ms/ $INDIR
+  echo "q4"
+  INDIR=$SCRIPT_DIR/q4_boki/4src_3/180s_0swarm_100ms_src100ms/$i
+  [[ -d $INDIR ]] && latency scan --prefix q4Avg --suffix .json.gz --output $OUT_DIR/q4-180s-0swarm-100ms-src100ms/ $INDIR
+  echo "q5"
+  INDIR=$SCRIPT_DIR/q5_boki/mem/4src_3/180s_0swarm_100ms_src100ms/$i
+  [[ -d $INDIR ]] && latency scan --prefix q5maxbid --suffix .json.gz --output $OUT_DIR/q5-180s-0swarm-100ms-src100ms/ $INDIR
+  echo "q6"
+  INDIR=$SCRIPT_DIR/q6_boki/4src_3/180s_0swarm_100ms_src100ms/$i
+  [[ -d $INDIR ]] && latency scan --prefix q6Avg --suffix .json.gz --output $OUT_DIR/q6-180s-0swarm-100ms-src100ms/ $INDIR
+  echo "q7"
+  INDIR=$SCRIPT_DIR/q7_boki/mem_nosync/4src_3/180s_0swarm_100ms_src100ms_comm100ms/$i
+  [[ -d $INDIR ]] && latency scan --prefix q7JoinMaxBid --suffix .json.gz --output $OUT_DIR/q7-180s-0swarm-100ms-src100ms/ $INDIR
+  echo "q8"
+  INDIR=$SCRIPT_DIR/q8_boki/mem/4src_3/180s_0swarm_100ms_src100ms/$i
+  [[ -d $INDIR ]] && latency scan --prefix q8JoinStream --suffix .json.gz --output $OUT_DIR/q8-180s-0swarm-100ms-src100ms/ $INDIR
 done
