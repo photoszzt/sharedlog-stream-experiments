ecb7ab186e44fd0c05d8d497ecc5aa9b8e6f7de5
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-16 20:40:25 -0500

    add extra
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/parse_multi_stream_stats.py
+++ nexmark_sharedlog/parse_multi_stream_stats.py
@@ -4,13 +4,13 @@ import json
 import numpy as np
 from parse_epoch_mark_time import parse_single_topdir
 
-
+extra=200
 
 dirs = {
-    4: "./q8_boki/mem/4src_4node_4ins_0extra_kvrocks",  
-    8: "./q8_boki/mem_4node_8ins/8src_4node_8ins_0extra_kvrocks/",
-    16: "./q8_boki/mem_4node_16ins/16src_4node_16ins_0extra_kvrocks/",
-    32: "./q8_boki/mem_4node_32ins/32src_4node_32ins_0extra_kvrocks/",
+    4: f"./q8_boki/mem/4src_4node_4ins_{extra}extra_kvrocks",  
+    8: f"./q8_boki/mem_4node_8ins/8src_4node_8ins_{extra}extra_kvrocks/",
+    16: f"./q8_boki/mem_4node_16ins/16src_4node_16ins_{extra}extra_kvrocks/",
+    32: f"./q8_boki/mem_4node_32ins/32src_4node_32ins_{extra}extra_kvrocks/",
 }
 
 tps_map = {
@@ -44,16 +44,8 @@ def main():
         stats[i]['epoch'] = epoch_stat
         stats[i]['2pc'] = twopc_stat
     print(stats)
-    with open("q8_multistream_stats.json", "w") as f:
+    with open(f"q8_multistream_stats_{extra}extra.json", "w") as f:
         json.dump(stats, f, cls=NpEncoder)
-    # stages = stats[4]['epoch']['per_stage'].keys()
-    # fig = plt.figure(figsize=(7, 3), layout="constrained")
-    # for st in stages:
-    #     epoch_fa_p50 = [stats[i]['epoch']['flush_all']['per_stage'][st][tps_map[i]]['p50'] for i in ins]
-    #     epoch_fa_p99 = [stats[i]['epoch']['flush_all']['per_stage'][st][tps_map[i]]['p99'] for i in ins]
-    #     fig.plot(ins, epoch_fa_p50, label=f'{st} p50')
-    #     fig.plot(ins, epoch_fa_p99, label=f'{st} p99')
-    # plt.save("q8_flush_all.pdf")
 
 
 if __name__ == '__main__':
