f7878f7b627ecacd208317c7df499d48ccb1d7a2
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-20 16:46:32 -0500

    back up script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/run_q5_varflush.sh
+++ nexmark_sharedlog/run_q5_varflush.sh
@@ -2,24 +2,24 @@
 set -x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKSPACE_DIR=$(realpath $SCRIPT_DIR/../../)
-DIR=q6_boki
+DIR=q5_boki/mem
 
 cd $DIR
 $WORKSPACE_DIR/research-helper-scripts/microservice_helper start-machines --use-spot-instances
 ./update_docker.sh
-cd ..
+cd ../..
 
-# TPS_PER_WORKER=(250 500 750 1000 1250 1500)
-TPS_PER_WORKER=(750)
+#TPS_PER_WORKER=(1000 8000 16000 24000 32000 40000 48000 56000 64000)
+TPS_PER_WORKER=(48000)
 NUM_WORKER=(4)
 DURATION=180
 WARM_DURATION=0
-APP=q6
-# FLUSH_MS=100
+APP=q5
 SRC_FLUSH_MS=(25 50 100)
 SNAPSHOT_S=10
+# FLUSH_MS=100
 # COMM_EVERY_MS=100
-# modes=(epoch none 2pc align_chkpt)
+# modes=(align_chkpt epoch 2pc none)
 modes=(epoch 2pc)
 
 cd ${DIR}
@@ -28,17 +28,17 @@ for ((idx = 0; idx < ${#TPS_PER_WORKER[@]}; ++idx)); do
         FLUSH_MS=${SRC_FLUSH_MS[$s]}
         COMM_EVERY_MS=${SRC_FLUSH_MS[$s]}
         for ((w = 0; w < ${#NUM_WORKER[@]}; ++w)); do
-            TPS=$(expr ${TPS_PER_WORKER[$idx]} \* ${NUM_WORKER[$w]})
+            TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_WORKER[w]})
             EVENTS=$(expr $TPS \* $DURATION)
             echo ${APP}, ${DIR}, ${EVENTS} events, ${TPS} tps, $FLUSH_MS flush_ms, ${SRC_FLUSH_MS[$s]} srcflush_ms, $COMM_EVERY_MS comm_ms
             subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS[$s]}ms
             for mode in ${modes[@]}; do
-                for ((iter=2; iter < 4; ++iter)); do
+                for ((iter=0; iter < 1; ++iter)); do
                     ./run_once.sh --app ${APP} \
                         --exp_dir ./${NUM_WORKER[w]}src_4/${subdir}/${iter}/${TPS_PER_WORKER[idx]}tps_${mode}/ \
                         --gua $mode --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER[w]} \
                         --tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS \
-			--src_flushms ${SRC_FLUSH_MS[$s]} \
+		        --src_flushms $SRC_FLUSH_MS[$s] \
                         --snapshot_s ${SNAPSHOT_S} --comm_everyMs ${COMM_EVERY_MS}
                 done
             done
@@ -49,4 +49,4 @@ cd -
 
 cd $DIR
 $WORKSPACE_DIR/research-helper-scripts/microservice_helper stop-machines
-cd ..
+cd ../..
