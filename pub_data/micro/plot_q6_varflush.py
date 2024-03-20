a2ce0fafe7dc65f094079103764b855cb8222bd8
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-20 12:20:56 -0500

    backup q6 varflush
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- pub_data/micro/plot_q6_varflush.py
+++ pub_data/micro/plot_q6_varflush.py
@@ -9,10 +9,11 @@ sys.path.append(os.path.join(parent, 'pub_data'))
 
 from fig_const import markers, colors, headers
 
-markIntr = [3, 5, 7, 10]
-markFreq = [333, 200, 142, 100]
-epoch_dir = "../q8_varflush_stats/epoch/" 
-twopc_dir = "../q8_varflush_stats/2pc/"
+markFreq = [10 20 40]
+markIntr = [100 50 25]
+epoch_dir = "./q6_varflush/epoch"
+twopc_dir = "./q6_varflush/2pc"
+
 
 def main():
     epoch_p50 = []
@@ -20,23 +21,22 @@ def main():
     twopc_p50 = []
     twopc_p99 = []
     for intr in markIntr:
-        fname = os.path.join(epoch_dir, f"{intr}ms", "q8.json")
+        fname = os.path.join(epoch_dir, f"{intr}ms", "q6.json")
         with open(fname, "r") as f:
             data = json.load(f)
-        epoch_p50.append(data["12000"]["p50"])
-        epoch_p99.append(data["12000"]["p99"])
+        epoch_p50.append(data["750"]["p50"])
+        epoch_p99.append(data["750"]["p99"])
 
-        fname = os.path.join(twopc_dir, f"{intr}ms", "q8.json")
+        fname = os.path.join(twopc_dir, f"{intr}ms", "q6.json")
         with open(fname, "r") as f:
             data = json.load(f)
-        twopc_p50.append(data["12000"]["p50"])
-        twopc_p99.append(data["12000"]["p99"])
+        twopc_p50.append(data["750"]["p50"])
+        twopc_p99.append(data["750"]["p99"])
     print(f"mark intr: {markIntr}")
     print(f"epoch p50: {epoch_p50}")
     print(f"epoch p99: {epoch_p99}")
     print(f"twopc p50: {twopc_p50}")
     print(f"twopc p99: {twopc_p99}")
-
     fig = plt.figure(figsize=(6.5, 3))
     axs = fig.subplots(1, 2)
     l1, = axs[0].plot(markFreq, epoch_p50, label="Impeller p50", color=colors[0], marker=markers[0])
@@ -46,10 +46,9 @@ def main():
     axs[0].legend(loc=(0.3, 1.05), ncol=2, handles=[l1, l2, l3, l4])
     fig.supxlabel('Progress marking frequency(marks/s)', fontsize=14)
     fig.supylabel('Event time latency (ms)', fontsize=14)
-    axs[0].set_ylim(0, 150)
-    axs[1].set_ylim(0, 1100)
     plt.subplots_adjust(bottom=0.15)
-    plt.savefig('q8_varMarkTime.pdf', bbox_inches='tight')
+    plt.savefig('q6_varMarkTime.pdf', bbox_inches='tight')
+
 
 
 if __name__ == '__main__':
