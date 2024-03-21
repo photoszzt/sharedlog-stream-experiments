d55f1ae74e6d408764f9512c76dfdfc4e50bc056
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-21 11:03:52 -0500

    q7 can choose 12000
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/run_q7_varflush.sh
+++ nexmark_sharedlog/run_q7_varflush.sh
@@ -2,7 +2,7 @@
 set -x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKSPACE_DIR=$(realpath $SCRIPT_DIR/../../)
-DIR=q7_boki/mem
+DIR=q7_boki/mem_nosync
 
 cd $DIR
 $WORKSPACE_DIR/research-helper-scripts/microservice_helper start-machines --use-spot-instances
@@ -10,12 +10,12 @@ $WORKSPACE_DIR/research-helper-scripts/microservice_helper start-machines --use-
 cd ../..
 
 # TPS_PER_WORKER=(4000 8000 12000 16000 20000 24000 28000 32000 36000 40000)
-TPS_PER_WORKER=(32000)
+TPS_PER_WORKER=(8000 12000)
 NUM_WORKER=(4)
 DURATION=180
 WARM_DURATION=0
 APP=q7
-SRC_FLUSH_MS=(25 50 100)
+SRC_FLUSH_MS=(100 50 25)
 SNAPSHOT_S=10
 # FLUSH_MS=100
 # SRC_FLUSH_MS=100
@@ -34,7 +34,7 @@ for ((idx = 0; idx < ${#TPS_PER_WORKER[@]}; ++idx)); do
             echo ${APP}, ${DIR}, ${EVENTS} events, ${TPS} tps, $FLUSH_MS flush_ms, ${SRC_FLUSH_MS[$s]} srcflush_ms, $COMM_EVERY_MS comm_ms
             subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS[$s]}ms_comm${COMM_EVERY_MS}ms
             for mode in ${modes[@]}; do
-                for ((iter=0; iter < 1; ++iter)); do
+                for ((iter=0; iter < 2; ++iter)); do
 		    # echo ${NUM_WORKER[w]}src_4/${subdir}/${iter}/${TPS_PER_WORKER[idx]}tps_${mode}
                     ./run_once.sh --app ${APP} \
                         --exp_dir ./${NUM_WORKER[w]}src_4/${subdir}/${iter}/${TPS_PER_WORKER[idx]}tps_${mode}/ \
