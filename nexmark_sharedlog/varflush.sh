0cb3b897fedcaaff3fd34a6c1ca4a66371dddcad
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-21 14:51:30 -0500

    add varflush
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/varflush.sh
+++ nexmark_sharedlog/varflush.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 set -euo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-flushes=(100 50 25)
+flushes=(100 50 25 10)
 modes=(epoch 2pc)
 
 for ((i=0; i < ${#flushes[@]}; ++i)); do
   f=${flushes[i]}
   for ((j=0; j < ${#modes[@]}; ++j)); do
     m=${modes[j]}
-    python3 parse_e2e_latency.py \
-    	--dir $SCRIPT_DIR/q6_boki/4src_4/180s_0swarm_${f}ms_src${f}ms/ \
-    	--out_stats $SCRIPT_DIR/../pub_data/micro/q6_varflush/${m}/${f}ms/ --app q6 --target ${m}
-    python3 parse_e2e_latency.py \
-    	--dir $SCRIPT_DIR/q4_boki/4src_4/180s_0swarm_${f}ms_src${f}ms/ \
-    	--out_stats $SCRIPT_DIR/../pub_data/micro/q4_varflush/${m}/${f}ms/ --app q4 --target ${m}
-    python3 parse_e2e_latency.py \
-    	--dir $SCRIPT_DIR/q7_boki/mem_nosync/4src_4/180s_0swarm_${f}ms_src${f}ms_comm${f}ms/ \
-    	--out_stats $SCRIPT_DIR/../pub_data/micro/q7_varflush/${m}/${f}ms/ --app q7 --target ${m}
+    if [[ -d $SCRIPT_DIR/q6_boki/4src_4/180s_0swarm_${f}ms_src${f}ms/ ]]; then
+      python3 parse_e2e_latency.py \
+      	--dir $SCRIPT_DIR/q6_boki/4src_4/180s_0swarm_${f}ms_src${f}ms/ \
+      	--out_stats $SCRIPT_DIR/../pub_data/micro/q6_varflush/${m}/${f}ms/ --app q6 --target ${m}
+    fi
+    if [[ -d $SCRIPT_DIR/q4_boki/4src_4/180s_0swarm_${f}ms_src${f}ms/ ]]; then
+      python3 parse_e2e_latency.py \
+      	--dir $SCRIPT_DIR/q4_boki/4src_4/180s_0swarm_${f}ms_src${f}ms/ \
+      	--out_stats $SCRIPT_DIR/../pub_data/micro/q4_varflush/${m}/${f}ms/ --app q4 --target ${m}
+    fi
+    if [[ -d $SCRIPT_DIR/q7_boki/mem_nosync/4src_4/180s_0swarm_${f}ms_src${f}ms_comm${f}ms/ ]]; then
+      python3 parse_e2e_latency.py \
+      	--dir $SCRIPT_DIR/q7_boki/mem_nosync/4src_4/180s_0swarm_${f}ms_src${f}ms_comm${f}ms/ \
+      	--out_stats $SCRIPT_DIR/../pub_data/micro/q7_varflush/${m}/${f}ms/ --app q7 --target ${m}
+    fi
   done
 done
