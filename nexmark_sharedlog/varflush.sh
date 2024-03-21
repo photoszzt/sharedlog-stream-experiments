0af23c3abaf606813fce0d0ddd564e720efd91fd
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-03-21 11:01:46 -0500

    parse varflush
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark_sharedlog/varflush.sh
+++ nexmark_sharedlog/varflush.sh
@@ -11,5 +11,11 @@ for ((i=0; i < ${#flushes[@]}; ++i)); do
     python3 parse_e2e_latency.py \
     	--dir $SCRIPT_DIR/q6_boki/4src_4/180s_0swarm_${f}ms_src${f}ms/ \
     	--out_stats $SCRIPT_DIR/../pub_data/micro/q6_varflush/${m}/${f}ms/ --app q6 --target ${m}
+    python3 parse_e2e_latency.py \
+    	--dir $SCRIPT_DIR/q4_boki/4src_4/180s_0swarm_${f}ms_src${f}ms/ \
+    	--out_stats $SCRIPT_DIR/../pub_data/micro/q4_varflush/${m}/${f}ms/ --app q4 --target ${m}
+    python3 parse_e2e_latency.py \
+    	--dir $SCRIPT_DIR/q7_boki/mem_nosync/4src_4/180s_0swarm_${f}ms_src${f}ms_comm${f}ms/ \
+    	--out_stats $SCRIPT_DIR/../pub_data/micro/q7_varflush/${m}/${f}ms/ --app q7 --target ${m}
   done
 done
