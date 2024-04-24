0a21551e55a0226b58af23d26656909d44465e34
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-04-24 06:10:43 +0000

    install grpc tools
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- setup_machine/install_deps.sh
+++ setup_machine/install_deps.sh
@@ -57,3 +57,6 @@ sudo usermod -aG docker $USER
 curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v26.1/protoc-26.1-linux-x86_64.zip
 unzip protoc-26.1-linux-x86_64.zip -d $HOME/.local
 rm protoc-26.1-linux-x86_64.zip
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
