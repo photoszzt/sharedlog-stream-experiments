197210bc65106a1fca6148d6e620e0d5e781dd2a
zhiting zhu <zhitingz@cs.utexas.edu>
2024-05-25 20:20:07 -0500

    backup script to plot q8 var mark data
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- plot_fig/plot_q8_varmark.py
+++ plot_fig/plot_q8_varmark.py
@@ -9,10 +9,12 @@ sys.path.append(os.path.join(parent, 'pub_data'))
 
 from fig_const import markers, colors, headers
 
-markIntr = [3, 5, 7, 10]
-markFreq = [333, 200, 142, 100]
-epoch_dir = "../q8_varflush_stats/epoch/" 
-twopc_dir = "../q8_varflush_stats/2pc/"
+markIntr = [5, 7, 10, 25, 50]
+markFreq = [200, 142, 100, 40, 20]
+# epoch_dir = "../q8_varflush_stats/epoch/" 
+# twopc_dir = "../q8_varflush_stats/2pc/"
+epoch_dir = "../pub_data/micro/q8_varflush/epoch/" 
+r2pc_dir = "../pub_data/micro/q8_varflush/remote_2pc/"
 
 def main():
     epoch_p50 = []
@@ -26,7 +28,7 @@ def main():
         epoch_p50.append(data["12000"]["p50"])
         epoch_p99.append(data["12000"]["p99"])
 
-        fname = os.path.join(twopc_dir, f"{intr}ms", "q8.json")
+        fname = os.path.join(r2pc_dir, f"{intr}ms", "q8.json")
         with open(fname, "r") as f:
             data = json.load(f)
         twopc_p50.append(data["12000"]["p50"])
@@ -41,13 +43,13 @@ def main():
     axs = fig.subplots(1, 2)
     l1, = axs[0].plot(markFreq, epoch_p50, label="Impeller p50", color=colors[0], marker=markers[0])
     l2, = axs[1].plot(markFreq, epoch_p99, label="Impeller p99", color=colors[0], marker=markers[0], ls='--')
-    l3, = axs[0].plot(markFreq, twopc_p50, label="2pc on Impeller p50", color=colors[3], marker=markers[3])
-    l4, = axs[1].plot(markFreq, twopc_p99, label="2pc on Impeller p99", color=colors[3], marker=markers[3], ls='--')
-    axs[0].legend(loc=(0.3, 1.05), ncol=2, handles=[l1, l2, l3, l4])
+    l3, = axs[0].plot(markFreq, twopc_p50, label="Kafka Stream on Impeller p50", color=colors[3], marker=markers[3])
+    l4, = axs[1].plot(markFreq, twopc_p99, label="Kafka Stream on Impeller p99", color=colors[3], marker=markers[3], ls='--')
+    axs[0].legend(loc=(0.2, 1.05), ncol=2, handles=[l1, l2, l3, l4])
     fig.supxlabel('Progress marking frequency(marks/s)', fontsize=14)
     fig.supylabel('Event time latency (ms)', fontsize=14)
-    axs[0].set_ylim(0, 150)
-    axs[1].set_ylim(0, 1100)
+    axs[0].set_ylim(0, 80)
+    axs[1].set_ylim(0, 250)
     plt.subplots_adjust(bottom=0.15)
     plt.savefig('q8_varMarkTime.pdf', bbox_inches='tight')
 
