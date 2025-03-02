1a49e12bf6c5d5486859c1313492948818e69a4e
Zhiting Zhu <zhitingz@lepton.ai>
2025-03-01 18:18:11 -0800

    fix type
    Signed-off-by: Zhiting Zhu <zhitingz@lepton.ai>

--- pub_data/fig10_fig11/fig10_all.py
+++ pub_data/fig10_fig11/fig10_all.py
@@ -1,11 +1,16 @@
 import csv
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 import subprocess
 import sys
 import os
 import numpy as np
 from string import ascii_lowercase
+import matplotlib
+
+matplotlib.rcParams["pdf.fonttype"] = 42
+matplotlib.rcParams["ps.fonttype"] = 42
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -80,19 +85,31 @@ throughputs = [
     [4000, 8000, 12000, 16000, 20000, 24000, 28000, 32000, 36000],
 ]
 
+
 def load(system, experiment):
-    rows = subprocess.run(["./latency", "query", system[experiment]], stdout=subprocess.PIPE)
-    rows = rows.stdout.decode('utf-8').strip().split('\n')
-    rows = [row for row in csv.DictReader(rows, fieldnames=headers) 
-            if (row['del'] == 'eo' or row['del'] == '2pc' or row['del'] == 'align_chkpt' or row['del'] == 'remote_2pc') and int(row['tps']) in throughputs[experiment]]
-    rows.sort(key=lambda row: int(row['tps']))
+    rows = subprocess.run(
+        ["./latency", "query", system[experiment]], stdout=subprocess.PIPE
+    )
+    rows = rows.stdout.decode("utf-8").strip().split("\n")
+    rows = [
+        row
+        for row in csv.DictReader(rows, fieldnames=headers)
+        if (
+            row["del"] == "eo"
+            or row["del"] == "2pc"
+            or row["del"] == "align_chkpt"
+            or row["del"] == "remote_2pc"
+        )
+        and int(row["tps"]) in throughputs[experiment]
+    ]
+    rows.sort(key=lambda row: int(row["tps"]))
     return rows
 
+
 if __name__ == "__main__":
-    fig, axs = plt.subplots(4, 4, figsize=(28, 16), layout='constrained')
+    fig, axs = plt.subplots(4, 4, figsize=(28, 16), layout="constrained")
     handles = None
-    letters=[f'({i})' for i in ascii_lowercase]
-    print(axs)
+    letters = [f"({i})" for i in ascii_lowercase]
     sys_dict = {}
     kafka_dict = {}
     ktos_p50_min_ratio = None
@@ -114,45 +131,50 @@ if __name__ == "__main__":
         ackpt = load(alignchkpts, experiment)
         r2pc = load(remote_2pc, experiment)
 
-        sys_in_per_worker_tp = [int(row['tps']) for row in sys]
-        kafka_in_per_worker_tp = [int(row['tps']) for row in kafka]
-        ackpt_in_per_worker_tp = [int(row['tps']) for row in ackpt]
-        remote2pc_in_per_worker_tp = [int(row['tps']) for row in r2pc]
+        sys_in_per_worker_tp = [int(row["tps"]) for row in sys]
+        kafka_in_per_worker_tp = [int(row["tps"]) for row in kafka]
+        ackpt_in_per_worker_tp = [int(row["tps"]) for row in ackpt]
+        remote2pc_in_per_worker_tp = [int(row["tps"]) for row in r2pc]
         sys_p99_over1 = None
         kafka_p99_over1 = None
         for row in sys:
-            sys_dict[int(row['tps'])] = {"p50": int(row['p50']), "p99": int(row['p99'])}
-            if int(row['p99']) > 1000 and sys_p99_over1 is None:
-                sys_p99_over1 = int(row['tps'])
+            sys_dict[int(row["tps"])] = {"p50": int(row["p50"]), "p99": int(row["p99"])}
+            if int(row["p99"]) > 1000 and sys_p99_over1 is None:
+                sys_p99_over1 = int(row["tps"])
         for row in kafka:
-            kafka_dict[int(row['tps'])] = {"p50": int(row['p50']), "p99": int(row['p99'])}
-            if int(row['p99']) > 1000 and kafka_p99_over1 is None:
-                kafka_p99_over1 = int(row['tps'])
+            kafka_dict[int(row["tps"])] = {
+                "p50": int(row["p50"]),
+                "p99": int(row["p99"]),
+            }
+            if int(row["p99"]) > 1000 and kafka_p99_over1 is None:
+                kafka_p99_over1 = int(row["tps"])
         if sys_p99_over1 is None:
             sys_p99_over1 = sys_in_per_worker_tp[-1]
         if kafka_p99_over1 is None:
             kafka_p99_over1 = kafka_in_per_worker_tp[-1]
-        print('Q' + str(experiment + 1))
-        print(f"kafka p99 over 1s: {kafka_p99_over1}, epoch p99 over 1s: {sys_p99_over1}, ratio: {sys_p99_over1/kafka_p99_over1}")
+        print("Q" + str(experiment + 1))
+        print(
+            f"kafka p99 over 1s: {kafka_p99_over1}, epoch p99 over 1s: {sys_p99_over1}, ratio: {sys_p99_over1/kafka_p99_over1}"
+        )
 
-        kafka_in_tp = [i*4 for i in kafka_in_per_worker_tp]
-        sys_in_tp = [i*4 for i in sys_in_per_worker_tp]
-        ackpt_in_tp = [i*4 for i in ackpt_in_per_worker_tp]
-        r2pc_in_tp = [i*4 for i in remote2pc_in_per_worker_tp]
+        kafka_in_tp = [i * 4 for i in kafka_in_per_worker_tp]
+        sys_in_tp = [i * 4 for i in sys_in_per_worker_tp]
+        ackpt_in_tp = [i * 4 for i in ackpt_in_per_worker_tp]
+        r2pc_in_tp = [i * 4 for i in remote2pc_in_per_worker_tp]
 
-        sys_p50 = [int(row['p50']) for row in sys]
-        sys_p99 = [int(row['p99']) for row in sys]
-        twopc_p50 = np.array([int(row['p50']) for row in twopc])
-        twopc_p99 = np.array([int(row['p99']) for row in twopc])
-        ackpt_p50 = [int(row['p50']) for row in ackpt]
-        ackpt_p99 = [int(row['p99']) for row in ackpt]
-        kafka_p50 = [int(row['p50']) for row in kafka]
-        kafka_p99 = [int(row['p99']) for row in kafka]
+        sys_p50 = [int(row["p50"]) for row in sys]
+        sys_p99 = [int(row["p99"]) for row in sys]
+        twopc_p50 = np.array([int(row["p50"]) for row in twopc])
+        twopc_p99 = np.array([int(row["p99"]) for row in twopc])
+        ackpt_p50 = [int(row["p50"]) for row in ackpt]
+        ackpt_p99 = [int(row["p99"]) for row in ackpt]
+        kafka_p50 = [int(row["p50"]) for row in kafka]
+        kafka_p99 = [int(row["p99"]) for row in kafka]
 
-        r2pc_p50 = [int(row['p50']) for row in r2pc]
-        r2pc_p99 = [int(row['p99']) for row in r2pc]
+        r2pc_p50 = [int(row["p50"]) for row in r2pc]
+        r2pc_p99 = [int(row["p99"]) for row in r2pc]
 
-        print(f"sys tp: {sys_in_tp}") 
+        print(f"sys tp: {sys_in_tp}")
         print(f"sys p50: {sys_p50}")
         print(f"sys p99: {sys_p99}")
         sys_p50_arr = np.array(sys_p50)
@@ -163,8 +185,8 @@ if __name__ == "__main__":
         kafka_p50_ratio = []
         kafka_p99_ratio = []
         for tps in kafka_in_per_worker_tp:
-            p50_ratio = round(kafka_dict[tps]["p50"]/sys_dict[tps]["p50"], 1)
-            p99_ratio = round(kafka_dict[tps]["p99"]/sys_dict[tps]["p99"], 1)
+            p50_ratio = round(kafka_dict[tps]["p50"] / sys_dict[tps]["p50"], 1)
+            p99_ratio = round(kafka_dict[tps]["p99"] / sys_dict[tps]["p99"], 1)
             if kafka_dict[tps]["p50"] <= 1000 and experiment >= 2:
                 if ktos_p50_min_ratio is None:
                     ktos_p50_min_ratio = p50_ratio
@@ -185,8 +207,8 @@ if __name__ == "__main__":
         print(f"kafka/sys p99: {kafka_p99_ratio}")
         print(f"remote_2pc p50: {r2pc_p50}")
         print(f"remote_2pc p99: {r2pc_p99}")
-        r2pc_p50_ratio = np.array(r2pc_p50)/sys_p50_arr
-        r2pc_p99_ratio = np.array(r2pc_p99)/sys_p99_arr
+        r2pc_p50_ratio = np.array(r2pc_p50) / sys_p50_arr
+        r2pc_p99_ratio = np.array(r2pc_p99) / sys_p99_arr
         print(f"r2pc/sys p50: {r2pc_p50_ratio}")
         print(f"r2pc/sys p99: {r2pc_p99_ratio}")
         print(f"ackpt tp: {ackpt_in_tp}")
@@ -231,31 +253,98 @@ if __name__ == "__main__":
         assert len(sys_in_tp) == len(r2pc_in_tp)
         print(f"ratio to 1st p99: {[i/sys_p99[0] for i in sys_p99]}")
         if experiment == 0 or experiment == 1:
-            allow=0.25
+            allow = 0.25
         else:
-            allow=0.15
-        lat_max = sys_p99[0] * (1+allow)
+            allow = 0.15
+        lat_max = sys_p99[0] * (1 + allow)
         for idx, t in enumerate(sys_p99):
-            if t <= 400 and sys_p50[idx] <= 400 and t <= lat_max and abs(r2pc_p99_ratio[idx]-1) <= allow:
+            if (
+                t <= 400
+                and sys_p50[idx] <= 400
+                and t <= lat_max
+                and abs(r2pc_p99_ratio[idx] - 1) <= allow
+            ):
                 target_idx = idx
-        print(f"idx: {target_idx}, tp: {sys_in_per_worker_tp[target_idx]}, epoch_p50: {sys_p50[target_idx]}, epoch_p99: {sys_p99[target_idx]}")
+        print(
+            f"idx: {target_idx}, tp: {sys_in_per_worker_tp[target_idx]}, epoch_p50: {sys_p50[target_idx]}, epoch_p99: {sys_p99[target_idx]}"
+        )
 
-        marksize=14
+        marksize = 14
 
         r = (experiment // 4) * 2
         c = experiment % 4
         ax1 = axs[r][c]
-        ax2 = axs[r+1][c]
-        l1, = ax1.plot(sys_in_tp, sys_p50, label='Impeller p50',  marker=markers[0],color=colors[0], markersize=marksize)
-        l3, = ax2.plot(sys_in_tp, sys_p99, label='Impeller p99', ls='--', marker=markers[0], color=colors[0], markersize=marksize)
-        l2, = ax1.plot(kafka_in_tp, [int(row['p50']) for row in kafka], label='Kafka Streams p50',  marker=markers[1],color=colors[1], markersize=marksize)
-        l4, = ax2.plot(kafka_in_tp, [int(row['p99']) for row in kafka], label='Kafka Streams p99', ls='--', marker=markers[1], color=colors[1], markersize=marksize)
+        ax2 = axs[r + 1][c]
+        (l1,) = ax1.plot(
+            sys_in_tp,
+            sys_p50,
+            label="Impeller p50",
+            marker=markers[0],
+            color=colors[0],
+            markersize=marksize,
+        )
+        (l3,) = ax2.plot(
+            sys_in_tp,
+            sys_p99,
+            label="Impeller p99",
+            ls="--",
+            marker=markers[0],
+            color=colors[0],
+            markersize=marksize,
+        )
+        (l2,) = ax1.plot(
+            kafka_in_tp,
+            [int(row["p50"]) for row in kafka],
+            label="Kafka Streams p50",
+            marker=markers[1],
+            color=colors[1],
+            markersize=marksize,
+        )
+        (l4,) = ax2.plot(
+            kafka_in_tp,
+            [int(row["p99"]) for row in kafka],
+            label="Kafka Streams p99",
+            ls="--",
+            marker=markers[1],
+            color=colors[1],
+            markersize=marksize,
+        )
         # l5, = ax1.plot(sys_in_tp, twopc_p50, label='2pc on Impeller p50',  marker=markers[3], color=colors[3], markersize=marksize)
         # l6, = ax1.plot(sys_in_tp, twopc_p99, label='2pc on Impeller p99',  ls='--', marker=markers[3],color=colors[3], markersize=marksize)
-        l7, = ax1.plot(r2pc_in_tp, r2pc_p50, label='Kafka Streams transaction on Impeller p50',  marker=markers[3], color=colors[3], markersize=marksize)
-        l8, = ax2.plot(r2pc_in_tp, r2pc_p99, label='Kafka Streams transaction on Impeller p99',  ls='--', marker=markers[3],color=colors[3], markersize=marksize)
-        l11, = ax1.plot(ackpt_in_tp, ackpt_p50, label='Align chkpt on Impeller p50',  marker=markers[4], color=colors[4], markersize=marksize)
-        l12, = ax2.plot(ackpt_in_tp, ackpt_p99, label='Align chkpt on Impeller p99',  ls='--', marker=markers[4],color=colors[4], markersize=marksize)
+        (l7,) = ax1.plot(
+            r2pc_in_tp,
+            r2pc_p50,
+            label="Kafka Streams transaction on Impeller p50",
+            marker=markers[3],
+            color=colors[3],
+            markersize=marksize,
+        )
+        (l8,) = ax2.plot(
+            r2pc_in_tp,
+            r2pc_p99,
+            label="Kafka Streams transaction on Impeller p99",
+            ls="--",
+            marker=markers[3],
+            color=colors[3],
+            markersize=marksize,
+        )
+        (l11,) = ax1.plot(
+            ackpt_in_tp,
+            ackpt_p50,
+            label="Align chkpt on Impeller p50",
+            marker=markers[4],
+            color=colors[4],
+            markersize=marksize,
+        )
+        (l12,) = ax2.plot(
+            ackpt_in_tp,
+            ackpt_p99,
+            label="Align chkpt on Impeller p99",
+            ls="--",
+            marker=markers[4],
+            color=colors[4],
+            markersize=marksize,
+        )
         lines = [l1, l2, l3, l4, l7, l8, l11, l12]
         for l in lines:
             l.set_linewidth(3)
@@ -263,8 +352,8 @@ if __name__ == "__main__":
         # ax1.set_xlabel('input throughput(events/s)', fontsize=16)
         # ax1.set_ylabel('event time latency(ms)', fontsize=16)
         handles = [l1, l3, l2, l4, l7, l8, l11, l12]
-            # ax1.legend(loc=(3, 1.05), ncol=2, handles=[l1, l5, l2, l3, l6, l4], handlelength=3, fontsize=11)
-        ax1.set_title(f'{letters[experiment]} Q{experiment+1}', fontsize=22)
+        # ax1.legend(loc=(3, 1.05), ncol=2, handles=[l1, l5, l2, l3, l6, l4], handlelength=3, fontsize=11)
+        ax1.set_title(f"{letters[experiment]} Q{experiment+1}", fontsize=22)
         ax1.tick_params(labelsize=20)
         ax1.xaxis.set_major_formatter(ticker.EngFormatter(sep=""))
         ax2.tick_params(labelsize=20)
@@ -282,10 +371,16 @@ if __name__ == "__main__":
             ax2.set_ylim([0, 1000])
 
         # plt.title('Q' + str(experiment + 1))
-    fig.legend(ncol=4, handles=handles, fontsize=22, loc='upper center', bbox_to_anchor=(0.5, 1.08))
-    fig.supxlabel('Input throughput(events/s)', fontsize=22)
-    fig.supylabel('Event time latency(ms)', fontsize=22)
-    plt.savefig('q1-8.pdf', bbox_inches='tight', pad_inches = 0)
+    fig.legend(
+        ncol=4,
+        handles=handles,
+        fontsize=22,
+        loc="upper center",
+        bbox_to_anchor=(0.5, 1.08),
+    )
+    fig.supxlabel("Input throughput(events/s)", fontsize=22)
+    fig.supylabel("Event time latency(ms)", fontsize=22)
+    plt.savefig("q1-8.pdf", bbox_inches="tight", pad_inches=0)
     print(f"kafka/sys p50 min ratio: {ktos_p50_min_ratio}")
     print(f"kafka/sys p50 max ratio: {ktos_p50_max_ratio}")
     print(f"kafka/sys p99 min ratio: {ktos_p99_min_ratio}")
